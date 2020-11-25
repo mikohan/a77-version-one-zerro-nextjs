cbca709188d6ca8a8bc921892f04ae45e3db20d4
mikohan <angara99@gmail.com>
2020-11-25 16:06:15 +0300

    Trying scss work

--- src/layouts/Main.tsx
+++ src/layouts/Main.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
-import { Grid } from '@material-ui/core';
+import { Grid, Paper } from '@material-ui/core';
 import Head from 'next/head';
 import AppBar from '~/components/header/AppBar';
 import { createStyles, Theme, makeStyles } from '@material-ui/core';
+import '../../styles/MainLayoutStyles.module.scss';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       flexGrow: 1,
+    },
+    rootGrid: {
+      position: 'relative',
+      minHeight: '100vh',
+    },
+    main: {},
+    footerGrid: {
+      position: 'absolute',
+      left: 0,
+      bottom: 0,
+      minHeight: 50,
+      backgroundColor: theme.palette.primary.main,
     },
     paper: {
       padding: theme.spacing(2),
@@ -26,17 +39,24 @@ export default function MainLayout(props: any) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className={classes.root}>
-        <Grid container direction="column">
+        <Grid className="pageStylse" container direction="column">
           <Grid item>
             <AppBar />
           </Grid>
-          <Grid item container>
-            <Grid item xs={false} sm={2} />
-            <Grid item xs={12} sm={6}>
-              {props.children}
+          <Grid className={classes.main} item container>
+            <Grid container item xs={12} spacing={2}>
+              <Grid item sm={2}>
+                some content
+              </Grid>
+              <Grid item xs={12} sm={10}>
+                {' '}
+                <Paper>{props.children}</Paper>
+              </Grid>
             </Grid>
           </Grid>
-          <footer></footer>
+          <Grid className="footerStyle" item container>
+            <footer>some footer</footer>
+          </Grid>
         </Grid>
       </div>
     </React.Fragment>
