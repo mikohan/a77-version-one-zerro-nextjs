4d54d5f682647ba5580ce81a9104d73022531378
mikohan <angara99@gmail.com>
2020-11-28 19:17:15 +0300

    Revert to AppBar working

--- src/pages/car/index.tsx
+++ src/pages/car/index.tsx
@@ -1,41 +1,16 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { GetServerSideProps } from 'next';
 import axios from 'axios';
 import { vehiclesUrl } from '~/config';
 import { List, ListItem } from '@material-ui/core';
 import Link from 'next/link';
 import MainLayout from '~/layouts/Main';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-import { useRouter } from 'next/router';
-
-import { useDispatch, useSelector } from 'react-redux';
-import { getMakes } from '~/store/actions/categoriesAction';
-import { changeCarModel } from '~/store/actions/categoriesAction';
-import { asString } from '~/helpers';
 
 interface ICarProps {
   makes: string[];
 }
 
 function Car(props: ICarProps) {
-  const router = useRouter();
-  const carModel = asString(router.query.model || 'all');
-  const carMake = router.query.make || '';
-  let carHref = '/';
-  if (carModel) {
-    carHref = `/car/${carMake}/${carModel}`;
-  }
-  const dispatch = useDispatch();
-  const storeCarModel = useSelector((state: any) => {
-    return state.currentCar.carModel;
-  });
-  console.log('Store car model', storeCarModel);
-
-  useEffect(() => {
-    dispatch(changeCarModel(carModel));
-    dispatch(getMakes());
-  }, []);
-
   return (
     <div>
       <MainLayout>
