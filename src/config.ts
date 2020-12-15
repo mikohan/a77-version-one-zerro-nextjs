350b032005188bedaefd57c55ead4de136d10011
mikohan <angara99@gmail.com>
2020-12-15 11:49:13 +0300

    Changed urls in endpoint

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,15 @@
 export const apiUrl = 'http://localhost:8000/testcategory/';
 
-export const vehiclesUrl = `${apiUrl}products/vehicles/`;
+export const vehiclesUrl = `${apiUrl}testproducts/vehicles/`;
 
 export const productUrl = `http://localhost:8000/testcategory/products/products/kolenval/`;
 
-export const categoriesUrl = `http://localhost:8000/testcategory/categories/`;
+export const categoriesUrl = `http://localhost:8000/testcategory/testcategories/`;
 
-export const getModelsByMakeUrl = 'http://localhost:8000/testcategory/models/';
+export const getModelsByMakeUrl =
+  'http://localhost:8000/testcategory/testmodels/';
 
-export const makesUrl = 'http://localhost:8000/testcategory/makes/';
+export const makesUrl = 'http://localhost:8000/testcategory/testmakes/';
 
 export const cagegoriesUrl =
   'http://localhost:8000/testcategory/categories-for-testes/';
