45aa7ef98dce1864986602a1c6f8ff911ee0c91f
mikohan <angara99@gmail.com>
2020-12-17 12:53:53 +0300

    Added filter rendering but not working

--- src/components/filters/Filter.tsx
+++ src/components/filters/Filter.tsx
@@ -1,70 +1,118 @@
-import * as React from 'react';
-import classnames from 'classnames';
+import React, { useCallback } from 'react';
+import { ArrowRoundedDown12x7Svg } from '~/svg';
+import FilterCategory from '~/components/filters/FilterCategory';
+// import FilterVehicle from '~/components/filters/FilterVehicle';
+import {
+  getFilterValue,
+  isDefaultFilterValue,
+  serializeFilterValue,
+} from '~/services/filters';
+// import FilterRange from '~/components/filters/FilterRange';
+//import FilterCheck from '~/components/filters/FilterCheck';
+//import FilterRadio from '~/components/filters/FilterRadio';
+//import FilterRating from '~/components/filters/FilterRating';
+//import FilterColor from '~/components/filters/FilterColor';
+import Collapse, { ICollapseRenderFn } from '~/components/shared/Collapse';
+import { IFilter } from '~/interfaces/filters';
+//import { useShopSetFilterValueThunk } from '~/store/shop/shopHooks';
 
-import styles from './styles.module.css';
+type RenderFilterFn = ICollapseRenderFn<HTMLDivElement, HTMLDivElement>;
 
-class Filter extends React.Component {
-  render() {
-    const containerClasses = classnames('container', 'mb-1', styles.container);
-    const formClasses = classnames('form-horizontal', styles.form);
-
-    return (
-      <div className={containerClasses}>
-        <form className={formClasses} noValidate>
-          <p className="mb-1">Refine your results</p>
-          <div className="columns text-center">
-            <div className="column col-4 col-xs-12">
-              <div className="form-group">
-                <div className="col-3 col-sm-12">
-                  <label className="form-label" htmlFor="price-from">
-                    Price from
-                  </label>
-                </div>
-                <div className="col-9 col-sm-12">
-                  <input
-                    className="form-input"
-                    min="0"
-                    max="10000000"
-                    type="number"
-                    id="price-from"
-                    placeholder="£1,000,000"
-                  />
-                </div>
-              </div>
-            </div>
-            <div className="column col-4 col-xs-12">
-              <div className="form-group">
-                <div className="col-3 col-sm-12">
-                  <label className="form-label" htmlFor="postcode">
-                    Postcode
-                  </label>
-                </div>
-                <div className="col-9 col-sm-12">
-                  <select className="form-select" id="postcode">
-                    <option value="">Choose...</option>
-                  </select>
-                </div>
-              </div>
-            </div>
-            <div className="column col-4 col-xs-12">
-              <div className="form-group">
-                <div className="col-3 col-sm-12">
-                  <label className="form-label" htmlFor="sortorder">
-                    Sort Order
-                  </label>
-                </div>
-                <div className="col-9 col-sm-12">
-                  <select className="form-select" id="sortorder">
-                    <option value="">Choose...</option>
-                  </select>
-                </div>
-              </div>
-            </div>
-          </div>
-        </form>
-      </div>
-    );
-  }
+interface ChangeValueEvent {
+  filter: IFilter;
+  value: IFilter['value'];
 }
 
-export default Filter;
+interface Props {
+  filter: IFilter;
+  value: string;
+}
+
+function Filter(props: Props) {
+  const { filter, value } = props;
+  // const shopSetFilterValue = useShopSetFilterValueThunk();
+
+  const handleValueChange = useCallback(
+    ({ filter, value }: ChangeValueEvent) => {
+      shopSetFilterValue(
+        filter.slug,
+        isDefaultFilterValue(filter, value)
+          ? null
+          : serializeFilterValue(filter, value)
+      ).then();
+    },
+    [shopSetFilterValue]
+  );
+
+  const renderFn: RenderFilterFn = ({ toggle, setItemRef, setContentRef }) => (
+    <div className="filter filter--opened" ref={setItemRef}>
+      <button type="button" className="filter__title" onClick={toggle}>
+        {filter.name}
+        <span className="filter__arrow">
+          <ArrowRoundedDown12x7Svg />
+        </span>
+      </button>
+      <div className="filter__body" ref={setContentRef}>
+        <div className="filter__container">
+          {filter.type === 'category' && <FilterCategory options={filter} />}
+
+          {/* {filter.type === 'vehicle' && ( */}
+          {/*   <FilterVehicle */}
+          {/*     options={filter} */}
+          {/*     value={getFilterValue(filter, value)} */}
+          {/*     onChangeValue={handleValueChange} */}
+          {/*   /> */}
+          {/* )} */}
+
+          {/* {filter.type === 'range' && ( */}
+          {/*   <FilterRange */}
+          {/*     options={filter} */}
+          {/*     value={getFilterValue(filter, value)} */}
+          {/*     onChangeValue={handleValueChange} */}
+          {/*   /> */}
+          {/* )} */}
+
+          {/* {filter.type === 'check' && ( */}
+          {/*   <FilterCheck */}
+          {/*     options={filter} */}
+          {/*     value={getFilterValue(filter, value)} */}
+          {/*     onChangeValue={handleValueChange} */}
+          {/*   /> */}
+          {/* )} */}
+
+          {/* {filter.type === 'radio' && ( */}
+          {/*   <FilterRadio */}
+          {/*     options={filter} */}
+          {/*     value={getFilterValue(filter, value)} */}
+          {/*     onChangeValue={handleValueChange} */}
+          {/*   /> */}
+          {/* )} */}
+
+          {/* {filter.type === 'rating' && ( */}
+          {/*   <FilterRating */}
+          {/*     options={filter} */}
+          {/*     value={getFilterValue(filter, value)} */}
+          {/*     onChangeValue={handleValueChange} */}
+          {/*   /> */}
+          {/* )} */}
+
+          {/* {filter.type === 'color' && ( */}
+          {/*   <FilterColor */}
+          {/*     options={filter} */}
+          {/*     value={getFilterValue(filter, value)} */}
+          {/*     onChangeValue={handleValueChange} */}
+          {/*   /> */}
+          {/* )} */}
+        </div>
+      </div>
+    </div>
+  );
+
+  return (
+    <div className="widget-filters__item">
+      <Collapse toggleClass="filter--opened" render={renderFn} />
+    </div>
+  );
+}
+
+export default React.memo(Filter);
