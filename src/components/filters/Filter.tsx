3fa03acd2e29b53468e1fb1cce59da51f6045339
mikohan <angara99@gmail.com>
2020-12-18 11:59:46 +0300

    Trying to add svg support

--- src/components/filters/Filter.tsx
+++ src/components/filters/Filter.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Box } from '@material-ui/core';
+import { ArrowRoundedDown12x7Svg } from '~/svg';
 
 import { IFilter } from '~/interfaces/filters';
 import FilterCategory from '~/components/filters/FilterCategory';
@@ -12,6 +13,14 @@ interface IProps {
 export default function Filter({ filter, value }: IProps) {
   return (
     <Box>
+      <Box>
+        <button type="button" className="filter__title">
+          {filter.name}
+          <span className="filter__arrow">
+            <ArrowRoundedDown12x7Svg />
+          </span>
+        </button>
+      </Box>
       {filter.type === 'category' && <FilterCategory options={filter} />}
     </Box>
   );
