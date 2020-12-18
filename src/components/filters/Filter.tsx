af9033e9da0e11e24c0e4339f1ce0eedfde6bf76
mikohan <angara99@gmail.com>
2020-12-18 17:25:48 +0300

    Added cheatcode

--- src/components/filters/Filter.tsx
+++ src/components/filters/Filter.tsx
@@ -28,7 +28,7 @@ function Filter(props: IProps) {
 
   //
   //
-
+  //
   const renderFn: RenderFilterFn = ({ toggle, setItemRef, setContentRef }) => (
     <div className="filter filter--opened" ref={setItemRef}>
       <Button
