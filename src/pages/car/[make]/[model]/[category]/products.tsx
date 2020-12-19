d68260c39664becf0491fa569f32b50b53baf8b4
mikohan <angara99@gmail.com>
2020-12-19 12:55:55 +0300

    Get static paths done

--- src/pages/car/[make]/[model]/[category]/products.tsx
+++ src/pages/car/[make]/[model]/[category]/products.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import MainLayout from '~/layouts/Main';
-import { Typography, Grid, Box } from '@material-ui/core';
+import { Typography, Grid, Box, ListItem, List } from '@material-ui/core';
 import FilterWidget from '~/components/main/FilterWidget';
 import LeftSideBar from '~/components/main/LeftSideBar';
 import { GetStaticPaths, GetStaticProps } from 'next';
 import axios from 'axios';
-import { productUrl } from '~/config';
+import { categoriesUrl, productUrl, vehiclesUrl } from '~/config';
 import { asString } from '~/helpers';
+import { IProduct } from '~/interfaces/product';
 
-export default function Products() {
+interface IProps {
+  products: IProduct[];
+}
+
+export default function Products({ products }: IProps) {
+  console.log(products);
   return (
     <div>
       <MainLayout>
@@ -20,6 +26,7 @@ export default function Products() {
         <Grid item xs={12} sm={9}>
           <Grid item xs={6}>
             <Typography variant="h1">Here some text</Typography>
+            <List></List>
           </Grid>
           <Grid item xs={6}></Grid>
         </Grid>
@@ -32,18 +39,38 @@ export const getStaticProps: GetStaticProps = async (context) => {
   const categorySlug = asString(context?.params?.category as string);
   const url = `${productUrl}${categorySlug}/`;
   const res = await axios.get(url);
-  console.log(res.data);
 
   return {
-    props: {},
+    props: {
+      products: res.data,
+    },
   };
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
   /* const res = await axios.get(a); */
+  const makeRes = await axios.get(vehiclesUrl);
+  const productsRes = await axios.get(productUrl);
+  const catRes = await axios.get(categoriesUrl);
+
+  const paths = [];
+  for (let vehicle of makeRes.data) {
+    for (let cat of catRes.data) {
+      for (let prod of productsRes.data) {
+        paths.push({
+          params: {
+            make: vehicle.make,
+            model: vehicle.slug,
+            category: cat.slug,
+            product: prod.slug,
+          },
+        });
+      }
+    }
+  }
 
   return {
     fallback: true,
-    paths: [],
+    paths: paths,
   };
 };
