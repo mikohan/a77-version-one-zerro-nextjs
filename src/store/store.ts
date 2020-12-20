2126a91692f1fd9c6f4bb8786289c2b7f1ec8877
mikohan <angara99@gmail.com>
2020-12-20 15:48:52 +0300

    Trying set store

--- src/store/store.ts
+++ src/store/store.ts
@@ -3,8 +3,18 @@ import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 import rootReducer from './reducers';
+import { ICar } from '~/interfaces/ICar';
+import { IMake } from '~/interfaces/IState';
 
 const initialState = {};
+export interface IState {
+  cars: {
+    cars: ICar[];
+    currentCar: ICar;
+    makes: IMake[];
+  };
+  currentCar: ICar;
+}
 
 const middleware = [thunk];
 
