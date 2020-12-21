151290b925aa38d3545b5fe3d0255fcb3af3c9fe
mikohan <angara99@gmail.com>
2020-12-21 12:43:58 +0300

    Start working for today

--- src/components/shared/TestListWidget.tsx
+++ src/components/shared/TestListWidget.tsx
@@ -1,17 +1,8 @@
 import React from 'react';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
-import ListSubheader from '@material-ui/core/ListSubheader';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
 import Collapse from '@material-ui/core/Collapse';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import DraftsIcon from '@material-ui/icons/Drafts';
-import SendIcon from '@material-ui/icons/Send';
-import ExpandLess from '@material-ui/icons/ExpandLess';
-import ExpandMore from '@material-ui/icons/ExpandMore';
-import StarBorder from '@material-ui/icons/StarBorder';
 import { IShopCategory } from '~/interfaces/category';
 import { Button, Typography } from '@material-ui/core';
 
