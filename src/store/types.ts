1e7f90c0299cc29a8f742d3d1eb47cfa8f3a87d8
mikohan <angara99@gmail.com>
2020-12-21 15:09:58 +0300

    Action added index file

--- src/store/types.ts
+++ src/store/types.ts
@@ -3,3 +3,5 @@ export const CHANGE_CAR_MODEL = 'CHANGE_CAR_MODEL';
 export const GET_MAKES = 'GET_MAKES';
 export const GET_MODELS = 'GET_MODELS';
 export const SELECT_BRAND = 'SELECT_BRAND';
+
+export type IActions = any;
