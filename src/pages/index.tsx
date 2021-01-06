092bfc186fe363d3ade40c105ba86d8233dd5138
mikohan <angara99@gmail.com>
2021-01-06 16:17:59 +0300

    Plan setting up

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,6 +13,7 @@
 // here is the plan in future
 // here is the plan in future
 // here is the plan in future
+// here is the plan in future
 
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 
