0478a599e27f380ec6e10b5563a93558b1656e1e
mikohan <angara99@gmail.com>
2021-03-15 10:27:23 +0300

    Working on graphql

--- src/endpoints/apolloClient.ts
+++ src/endpoints/apolloClient.ts
@@ -1,4 +1,24 @@
-import { ApolloClient, InMemoryCache, DefaultOptions } from '@apollo/client';
+import {
+  ApolloClient,
+  InMemoryCache,
+  DefaultOptions,
+  HttpLink,
+  from,
+} from '@apollo/client';
+import { onError } from '@apollo/client/link/error';
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+    graphQLErrors?.map(({ message, location, path }) => {
+      console.log(`Graphql Error: ${message}`);
+    });
+  }
+});
+
+const link = from([
+  errorLink,
+  new HttpLink({ uri: 'http://localhost:8000/product/graphql' }),
+]);
 
 const defaultOptions: DefaultOptions = {
   watchQuery: {
@@ -12,7 +32,6 @@ const defaultOptions: DefaultOptions = {
 };
 
 export const client = new ApolloClient({
-  uri: 'http://localhost:8000/product/graphql',
+  link: link,
   cache: new InMemoryCache(),
-  defaultOptions,
 });
