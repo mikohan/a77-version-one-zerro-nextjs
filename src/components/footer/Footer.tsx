e80e4cffd1203957a6fa8d471d17b5a38f4aafb9
mikohan <angara99@gmail.com>
2021-03-21 11:16:15 +0300

    Refactor grid layout

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,16 +3,17 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import Link from '@material-ui/core/Link';
+import Link from 'next/link';
+import MLink from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary">
       {'Copyright © '}
-      <Link color="inherit" href="/">
+      <MLink color="inherit" href="/">
         Your Website
-      </Link>{' '}
+      </MLink>{' '}
       {new Date().getFullYear()}
       {'.'}
     </Typography>
@@ -30,7 +31,7 @@ const useStyles = makeStyles((theme) => ({
     position: 'absolute',
   },
   gridItem: {
-    margin: '3rem',
+    margin: '6rem',
   },
 }));
 
@@ -44,28 +45,48 @@ export default function StickyFooter() {
         <Grid container justify="center" className={classes.mainGridContainer}>
           <Grid item className={classes.gridItem}>
             <Grid container direction="column" spacing={2}>
-              <Grid item>Home</Grid>
+              <Grid item component={Link} href="/">
+                Home
+              </Grid>
             </Grid>
           </Grid>
           <Grid item className={classes.gridItem}>
             <Grid container direction="column" spacing={2}>
-              <Grid item>Contacts</Grid>
-              <Grid item>About</Grid>
-              <Grid item>Policy</Grid>
+              <Grid item component={Link} href="/">
+                Contacts
+              </Grid>
+              <Grid item component={Link} href="/">
+                About
+              </Grid>
+              <Grid item component={Link} href="/">
+                Policy
+              </Grid>
             </Grid>
           </Grid>
           <Grid item className={classes.gridItem}>
             <Grid container direction="column" spacing={2}>
-              <Grid item>Some page</Grid>
-              <Grid item>Some other page</Grid>
-              <Grid item>Some yet anoter page</Grid>
+              <Grid item component={Link} href="/">
+                Some page
+              </Grid>
+              <Grid item component={Link} href="/">
+                Some other page
+              </Grid>
+              <Grid item component={Link} href="/">
+                Some yet anoter page
+              </Grid>
             </Grid>
           </Grid>
           <Grid item className={classes.gridItem}>
             <Grid container direction="column" spacing={2}>
-              <Grid item>About Us</Grid>
-              <Grid item>Team</Grid>
-              <Grid item>Contact Us</Grid>
+              <Grid item component={Link} href="/about">
+                About Us
+              </Grid>
+              <Grid item component={Link} href="/team">
+                Team
+              </Grid>
+              <Grid item component={Link} href="/contacts">
+                Contact Us
+              </Grid>
             </Grid>
           </Grid>
         </Grid>
