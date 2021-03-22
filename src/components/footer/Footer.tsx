35515c89b5056d3aba15d10bc7f5ce727dec0eff
mikohan <angara99@gmail.com>
2021-03-22 11:19:01 +0300

    Refactor active page to redux

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -9,6 +9,8 @@ import Grid from '@material-ui/core/Grid';
 import { useDispatch, useSelector } from 'react-redux';
 import { SET_ACTIVE_PAGE } from '~/store/types';
 import { IState } from '~/interfaces/IState';
+import { Hidden } from '@material-ui/core';
+import YouTubeIcon from '@material-ui/icons/YouTube';
 
 function Copyright() {
   return (
@@ -24,14 +26,14 @@ function Copyright() {
 }
 
 const useStyles = makeStyles((theme) => ({
-  root: { minHeight: '8rem' },
+  root: { minHeight: '8rem', position: 'relative' },
   /* main: { */
   /*   marginTop: theme.spacing(8), */
   /*   marginBottom: theme.spacing(2), */
   /* }, */
   footer: {},
   mainGridContainer: {
-    position: 'absolute',
+    // position: 'absolute',
   },
   gridItem: {
     margin: '6rem',
@@ -45,52 +47,87 @@ export default function StickyFooter() {
     <div className={classes.root}>
       <CssBaseline />
       <footer className={classes.footer}>
-        <Grid container justify="center" className={classes.mainGridContainer}>
-          <Grid item className={classes.gridItem}>
-            <Grid container direction="column" spacing={2}>
-              <Grid item component={Link} href="/">
-                Home
+        <Hidden smDown>
+          <Grid
+            container
+            justify="center"
+            className={classes.mainGridContainer}
+          >
+            <Grid item className={classes.gridItem}>
+              <Grid container direction="column" spacing={2}>
+                <Grid item component={Link} href="/">
+                  Home
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid item className={classes.gridItem}>
+              <Grid container direction="column" spacing={2}>
+                <Grid item component={Link} href="/contacts">
+                  Contacts
+                </Grid>
+                <Grid item component={Link} href="/">
+                  About
+                </Grid>
+                <Grid item component={Link} href="/">
+                  Policy
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid item className={classes.gridItem}>
+              <Grid container direction="column" spacing={2}>
+                <Grid item component={Link} href="/">
+                  Some page
+                </Grid>
+                <Grid item component={Link} href="/">
+                  Some other page
+                </Grid>
+                <Grid item component={Link} href="/">
+                  Some yet anoter page
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid item className={classes.gridItem}>
+              <Grid container direction="column" spacing={2}>
+                <Grid item component={Link} href="/about">
+                  About Us
+                </Grid>
+                <Grid item component={Link} href="/grid">
+                  Team
+                </Grid>
+                <Grid item component={Link} href="/contacts">
+                  Contact Us
+                </Grid>
               </Grid>
             </Grid>
           </Grid>
-          <Grid item className={classes.gridItem}>
-            <Grid container direction="column" spacing={2}>
-              <Grid item component={Link} href="/contacts">
-                Contacts
-              </Grid>
-              <Grid item component={Link} href="/">
-                About
-              </Grid>
-              <Grid item component={Link} href="/">
-                Policy
-              </Grid>
-            </Grid>
+        </Hidden>
+        <Grid container>
+          <Grid
+            item
+            component={'a'}
+            href="https://vk.com/angara772018"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            VK
           </Grid>
-          <Grid item className={classes.gridItem}>
-            <Grid container direction="column" spacing={2}>
-              <Grid item component={Link} href="/">
-                Some page
-              </Grid>
-              <Grid item component={Link} href="/">
-                Some other page
-              </Grid>
-              <Grid item component={Link} href="/">
-                Some yet anoter page
-              </Grid>
-            </Grid>
+          <Grid
+            item
+            component={'a'}
+            href="http://ok.ru"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            VK
           </Grid>
-          <Grid item className={classes.gridItem}>
-            <Grid container direction="column" spacing={2}>
-              <Grid item component={Link} href="/about">
-                About Us
-              </Grid>
-              <Grid item component={Link} href="/grid">
-                Team
-              </Grid>
-              <Grid item component={Link} href="/contacts">
-                Contact Us
-              </Grid>
-            </Grid>
+          <Grid
+            item
+            component={'a'}
+            href="http://youtube.com"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            YouTubeIcon
           </Grid>
         </Grid>
       </footer>
