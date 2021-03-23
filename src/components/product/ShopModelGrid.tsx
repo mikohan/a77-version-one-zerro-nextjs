beee1a6efd9e16a8871fdcfb8bf3aac545f7225c
mikohan <angara99@gmail.com>
2021-03-23 19:08:35 +0300

    Added car model and car make grids

---
+++ src/components/product/ShopModelGrid.tsx
@@ -0,0 +1,64 @@
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import Link from 'next/link';
+import { Typography } from '@material-ui/core';
+import { ICar } from '~/interfaces/ICar';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    cards: {
+      margin: '0 auto',
+      padding: theme.spacing(2),
+      display: 'grid',
+      gridTemplateColumns: `repeat(auto-fill, minmax(125px, 1fr))`,
+      gridAutoRows: `minmax(200px, auto)`,
+      gridGap: theme.spacing(2), // padding for cards in the content area
+      marginBottom: theme.spacing(5),
+    },
+    card: {
+      boxShadow: '1px 1px 7px 1px rgba(0, 0, 0, 0.1)',
+      borderRadius: '2px',
+      background: 'white',
+    },
+    cardContent: {
+      padding: theme.spacing(3),
+    },
+    cardImageLink: {
+      display: 'block',
+    },
+    productName: {
+      fontSize: '1.1rem',
+      color: theme.palette.grey[700],
+    },
+    productSku: {
+      paddingLeft: theme.spacing(2),
+      color: theme.palette.grey[500],
+    },
+  })
+);
+
+interface IProps {
+  cars: ICar[];
+}
+
+export default function ShopCarGrid({ cars }: IProps) {
+  const classes = useStyles();
+  return (
+    <div className={classes.cards}>
+      {cars.map((item: ICar) => {
+        return (
+          <div key={item.id} className={classes.card}>
+            <Link href={`/car/${item.slug}`}>
+              <a className={classes.cardImageLink}>
+                <div className={classes.cardContent}>
+                  <Typography className={classes.productName} variant="h6">
+                    {item.model}
+                  </Typography>
+                </div>
+              </a>
+            </Link>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
