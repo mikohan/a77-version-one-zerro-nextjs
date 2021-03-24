2b7b137eefb2b96ebae0b46b7540962e23ccf6c2
mikohan <angara99@gmail.com>
2021-03-24 19:16:19 +0300

    Start adding grid layout changes

--- src/components/product/ShopGrid.tsx
+++ src/components/product/ShopGrid.tsx
@@ -16,6 +16,7 @@ import { SET_SHOP_GRID, SET_SORT_VALUE } from '~/store/types';
 import { compareByNameAsc, compareByNameDesc } from '~/utils';
 import { useEffect } from 'react';
 import { useTheme } from '@material-ui/core/styles';
+import ProductCardGrid from './ProductCardGrid';
 
 interface IProps {
   products: IProductElasticHitsFirst;
@@ -38,60 +39,6 @@ export default function ShopGrid({ products }: IProps) {
             : `repeat(auto-fill, 1fr )`,
         gridGap: theme.spacing(4), // padding for cards in the content area
         marginBottom: theme.spacing(5),
-      },
-      card: {
-        position: 'relative',
-        display: 'block',
-        boxShadow: '0 1px 3px  rgba(0, 0, 0, 0.1)',
-        borderRadius: '2px',
-        background: 'white',
-        transition: '0.5s',
-        '&:hover $shoppingCartIcon': {
-          transform: `scale(1.3)`,
-          color: theme.palette.primary.main,
-          cursor: 'pointer',
-        },
-      },
-      cardImage: {
-        display: 'block',
-        position: 'absolute',
-        width: '100%',
-        height: '100%',
-        objectFit: 'cover', // contain maki it small, cover make it big
-      },
-      cardContent: {
-        minHeight: '8rem',
-        lineHeight: '1.5',
-        padding: theme.spacing(3),
-      },
-      cardInfo: {
-        paddingLeft: theme.spacing(2),
-        paddingRight: theme.spacing(2),
-        paddingTop: theme.spacing(2),
-        paddingBottom: theme.spacing(2),
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        /* background: theme.palette.grey[200], */
-      },
-      shoppingCartIcon: {
-        fontSize: '2rem',
-        color: theme.palette.grey[500],
-        transition: '0.2s',
-      },
-      cardImageLink: {
-        display: 'block',
-        position: 'relative',
-        width: '100%',
-        paddingBottom: '100%',
-      },
-      productName: {
-        fontSize: '1.1rem',
-        color: theme.palette.grey[700],
-      },
-      productSku: {
-        paddingLeft: theme.spacing(2),
-        color: theme.palette.grey[500],
       },
       pageBarContainer: {
         paddingLeft: theme.spacing(2),
@@ -219,38 +166,7 @@ export default function ShopGrid({ products }: IProps) {
       <Grid item xs={12}>
         <div className={classes.cards}>
           {products.hits.map((item: IProductElasticHitsSecond) => {
-            const imgPath: string = item._source.images.length
-              ? (item._source.images[0].img500 as string)
-              : '/images/local/defaultParts500.jpg';
-            return (
-              <div key={item._id} className={classes.card}>
-                <a className={classes.cardImageLink}>
-                  <img
-                    src={imgPath}
-                    className={classes.cardImage}
-                    alt="Some image"
-                  />
-                </a>
-                <div className={classes.cardContent}>
-                  <Typography className={classes.productName} variant="h6">
-                    {item._source.name}
-                  </Typography>
-                </div>
-                <div className={classes.productSku}>
-                  <Typography variant="body2">
-                    SKU: {item._source.cat_number}
-                  </Typography>
-                </div>
-                <div className={classes.cardInfo}>
-                  <Typography variant="h6">$ 450.00</Typography>
-                  <div>
-                    <ShoppingCartOutlinedIcon
-                      className={classes.shoppingCartIcon}
-                    />
-                  </div>
-                </div>
-              </div>
-            );
+            return <ProductCardGrid key={item._id} product={item} />;
           })}
         </div>
       </Grid>
