1b674457b629371e77c3319dc946b21b5675d54f
mikohan <angara99@gmail.com>
2021-03-25 13:23:03 +0300

    Drawer done

--- src/components/product/FilterDrawer.tsx
+++ src/components/product/FilterDrawer.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import clsx from 'clsx';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
 import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
@@ -8,17 +8,28 @@ import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
+import HighlightOffIcon from '@material-ui/icons/HighlightOff';
+import Box from '@material-ui/core/Box';
 
-const useStyles = makeStyles({
-  list: {
-    width: 350,
-  },
-  fullList: {
-    width: 'auto',
-  },
-});
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    list: {
+      width: 350,
+    },
+    fullList: {
+      width: 'auto',
+    },
+    closeBox: {
+      display: 'flex',
+      justifyContent: 'flex-end',
+      padding: theme.spacing(3),
+      color: theme.palette.grey[500],
+    },
+    closeIcon: {
+      fontSize: '2rem',
+    },
+  })
+);
 
 interface IProps {
   openDrawer: boolean;
@@ -39,8 +50,13 @@ export default function SwipeableTemporaryDrawer({
           onClose={toggleDrawer}
           onOpen={toggleDrawer}
         >
+          <Box className={classes.closeBox}>
+            <HighlightOffIcon className={classes.closeIcon} />
+          </Box>
           <List className={classes.list}>
-            <ListItem>some content </ListItem>
+            <ListItem>
+              <ListItemText>TEXT</ListItemText>
+            </ListItem>
           </List>
         </SwipeableDrawer>
       </React.Fragment>
