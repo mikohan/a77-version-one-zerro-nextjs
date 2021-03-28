1f66de6577e64c56be303a37b4b7ba07473414de
mikohan <angara99@gmail.com>
2021-03-28 09:45:14 +0300

    Breakpoints done

--- src/theme.tsx
+++ src/theme.tsx
@@ -2,22 +2,27 @@ import { createMuiTheme } from '@material-ui/core/styles';
 
 declare module '@material-ui/core/styles/createBreakpoints' {
   interface BreakpointOverrides {
-    xs: false; // removes the `xs` breakpoint
-    sm: false;
-    md: false;
-    lg: false;
-    xl: false;
+    xs: true; // removes the `xs` breakpoint
+    sm: true;
+    md: true;
+    lg: true;
+    xl: true;
     xxl: true;
   }
 }
 
 // Create a theme instance.
 const theme = createMuiTheme({
-  /* breakpoints: { */
-  /*   values: { */
-  /*     xxl: 2560, */
-  /*   }, */
-  /* }, */
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 960,
+      lg: 1280,
+      xl: 1920,
+      xxl: 2000,
+    },
+  },
   palette: {
     type: 'light',
   },
@@ -30,7 +35,7 @@ const theme = createMuiTheme({
   },
   typography: {
     h1: {
-      fontSize: '2rem',
+      fontSize: '1.5rem',
     },
     h2: {
       fontSize: '1.75rem',
@@ -40,5 +45,55 @@ const theme = createMuiTheme({
     },
   },
 });
+
+theme.typography.h1 = {
+  [theme.breakpoints.down('xl')]: {
+    fontFamily: `"Roboto", "Helvetica", "Arial", sans-serif`,
+    fontWeight: 300,
+    fontSize: '1.6rem',
+    lineHeight: 1.167,
+    letterSpacing: '-0.01562em',
+  },
+  [theme.breakpoints.up('xxl')]: {
+    fontFamily: `"Roboto", "Helvetica", "Arial", sans-serif`,
+    fontWeight: 300,
+    fontSize: '1.9rem',
+    lineHeight: 1.167,
+    letterSpacing: '-0.01562em',
+  },
+};
+theme.typography.body1 = {
+  [theme.breakpoints.up('xxl')]: {
+    fontFamily: `"Roboto", "Helvetica", "Arial", sans-serif"`,
+    fontWeight: 400,
+    fontSize: '1rem',
+    lineHeight: 1.5,
+    letterSpacing: '0.00938em',
+  },
+  [theme.breakpoints.down('xl')]: {
+    fontFamily: `"Roboto", "Helvetica", "Arial", sans-serif"`,
+    fontWeight: 400,
+    fontSize: '1.1rem',
+    lineHeight: 1.5,
+    letterSpacing: '0.00938em',
+  },
+};
+
+theme.typography.body2 = {
+  [theme.breakpoints.down('xl')]: {
+    fontFamily: `"Roboto", "Helvetica", "Arial", sans-serif"`,
+    fontWeight: 400,
+    fontSize: '0.875rem',
+    lineHeight: 1.43,
+    letterSpacing: '0.01071em',
+  },
+  [theme.breakpoints.up('xxl')]: {
+    fontFamily: `"Roboto", "Helvetica", "Arial", sans-serif"`,
+    fontWeight: 400,
+    fontSize: '1rem',
+    lineHeight: 1.5,
+    letterSpacing: '0.01071em',
+  },
+};
 
 export default theme;
