120ef558b8eb2bdb79d67399265274148432c2b4
mikohan <angara99@gmail.com>
2021-04-04 12:34:22 +0300

    Refactor check filters in url

--- src/services/filters/filterHandler.tsx
+++ src/services/filters/filterHandler.tsx
@@ -9,25 +9,6 @@ import {
 import { shopSetFilterVlue, shopDeleteFilter } from '~/store/shop/shopActions';
 import url from '~/services/url';
 
-export const handleFilterChange = (
-  e: React.ChangeEvent<HTMLInputElement>,
-  filterName: string,
-  itemName: string
-) => {
-  //router.push(urlPush);
-  /* if (des.includes(itemName)) { */
-  /*   // delete from des */
-  /*   const idx = des.indexOf(itemName); */
-  /*   des.splice(idx, 1); */
-  /* } else { */
-  /*   // add to des */
-  /*   des.push(itemName); */
-  /* } */
-  /* // serialize des and dispatch */
-  /* const newFilterValues = des.join(','); */
-  /* dispatch(shopSetFilterVlue(options.slug, newFilterValues)); */
-};
-
 // Function for redirection
 export function makePushUrl(
   router: NextRouter,
@@ -49,12 +30,14 @@ export function makePushUrl(
       params[value.filterSlug] = value.filterValues.join(',');
     }
   }
+  if (Object.keys(params).length > 0) {
+    params.filters_chk = '1';
+  }
 
   for (const item of activeFilters) {
     if (item.filterValues.length) {
       dispatch(shopSetFilterVlue(item.filterSlug, item.filterValues.join(',')));
     } else {
-      console.log('here is empty string', item.filterValues);
       dispatch(shopDeleteFilter(item.filterSlug));
     }
   }
