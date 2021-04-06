3882a44ff0b39267882c31cf2119b5f4e78c88d3
mikohan <angara99@gmail.com>
2021-04-06 15:53:54 +0300

    General layout done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,10 +7,29 @@ import { GetStaticProps } from 'next';
 import { IMake } from '~/interfaces/IMake';
 import { getMakes } from '~/endpoints/carsEndpoint';
 import Animation from '~/components/common/AnimationPage';
+import { Typography } from '@material-ui/core';
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    paper: {
-      padding: theme.spacing(2),
+    contentContainer: {
+      paddingTop: theme.spacing(3),
+      display: 'grid',
+      [theme.breakpoints.down('xxl')]: {
+        gridTemplateColumns: `repeat(auto-fill, minmax(90%, 1fr))`,
+      },
+
+      /* [theme.breakpoints.up('lg')]: { */
+      /*   gridTemplateColumns: `repeat(auto-fill, minmax(40%, 1fr))`, */
+      /*   gridGap: theme.spacing(3), */
+      /* }, */
+      '&> div': {
+        minHeight: '20rem',
+        padding: theme.spacing(3),
+        background: theme.palette.background.paper,
+        marginBottom: theme.spacing(3),
+      },
+    },
+    blockTitle: {
+      marginBottom: '2rem',
     },
   })
 );
@@ -26,20 +45,87 @@ export default function Home(props: IHomeProps) {
 
   return (
     <Animation>
-      <Paper className={classes.paper}>
-        <Grid container spacing={2}>
-          <Grid item xs={4}>
-            <ListForTesting makes={makes} />
-          </Grid>
-          <Grid item xs={4}>
-            Text
-          </Grid>
-          <Grid item xs={4}>
-            Text
-          </Grid>
-          <Link href="/testpage">To Test Page</Link>
+      <Grid container>
+        <Grid item xs={12}>
+          <div className={classes.contentContainer}>
+            <div>
+              <Typography variant="h6" className={classes.blockTitle}>
+                Расходники
+              </Typography>
+              <Typography variant="body1">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+                non odio reprehenderit illo facilis doloremque odit esse est
+                nemo alias assumenda eaque deserunt inventore quas hic sunt quae
+                nam, mollitia, dolorum, quia maiores eveniet? Unde enim laborum
+                veritatis possimus, odit vel maxime commodi, architecto
+                recusandae inventore ipsam, saepe sit provident reiciendis
+                accusamus rerum molestias voluptatem at dolor atque iure.
+                Voluptas?
+              </Typography>
+            </div>
+            <div>
+              <Typography variant="h6" className={classes.blockTitle}>
+                Машины
+              </Typography>
+              <Typography variant="body1">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+                non odio reprehenderit illo facilis doloremque odit esse est
+                nemo alias assumenda eaque deserunt inventore quas hic sunt quae
+                nam, mollitia, dolorum, quia maiores eveniet? Unde enim laborum
+                veritatis possimus, odit vel maxime commodi, architecto
+                recusandae inventore ipsam, saepe sit provident reiciendis
+                accusamus rerum molestias voluptatem at dolor atque iure.
+                Voluptas?
+              </Typography>
+            </div>
+            <div>
+              <Typography variant="h6" className={classes.blockTitle}>
+                Популярные товары
+              </Typography>
+              <Typography variant="body1">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+                non odio reprehenderit illo facilis doloremque odit esse est
+                nemo alias assumenda eaque deserunt inventore quas hic sunt quae
+                nam, mollitia, dolorum, quia maiores eveniet? Unde enim laborum
+                veritatis possimus, odit vel maxime commodi, architecto
+                recusandae inventore ipsam, saepe sit provident reiciendis
+                accusamus rerum molestias voluptatem at dolor atque iure.
+                Voluptas?
+              </Typography>
+            </div>
+            <div>
+              <Typography variant="h6" className={classes.blockTitle}>
+                Блог
+              </Typography>
+              <Typography variant="body1">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+                non odio reprehenderit illo facilis doloremque odit esse est
+                nemo alias assumenda eaque deserunt inventore quas hic sunt quae
+                nam, mollitia, dolorum, quia maiores eveniet? Unde enim laborum
+                veritatis possimus, odit vel maxime commodi, architecto
+                recusandae inventore ipsam, saepe sit provident reiciendis
+                accusamus rerum molestias voluptatem at dolor atque iure.
+                Voluptas?
+              </Typography>
+            </div>
+            <div>
+              <Typography variant="h6" className={classes.blockTitle}>
+                Videos
+              </Typography>
+              <Typography variant="body1">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+                non odio reprehenderit illo facilis doloremque odit esse est
+                nemo alias assumenda eaque deserunt inventore quas hic sunt quae
+                nam, mollitia, dolorum, quia maiores eveniet? Unde enim laborum
+                veritatis possimus, odit vel maxime commodi, architecto
+                recusandae inventore ipsam, saepe sit provident reiciendis
+                accusamus rerum molestias voluptatem at dolor atque iure.
+                Voluptas?
+              </Typography>
+            </div>
+          </div>
         </Grid>
-      </Paper>
+      </Grid>
     </Animation>
   );
 }
