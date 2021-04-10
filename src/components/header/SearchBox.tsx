118a6e2b6f26d09e23957c27277c26f8865f6b25
mikohan <angara99@gmail.com>
2021-04-10 16:37:42 +0300

    Refactor autocomplete component, working like it should

--- src/components/header/SearchBox.tsx
+++ src/components/header/SearchBox.tsx
@@ -1,13 +1,8 @@
 /* eslint-disable no-use-before-define */
-import React, { useEffect, useRef, useState } from 'react';
-import TextField from '@material-ui/core/TextField';
-import Autocomplete from '@material-ui/lab/Autocomplete';
-import { Grid, IconButton, Typography, Chip } from '@material-ui/core';
+import React from 'react';
+import { Grid, Chip } from '@material-ui/core';
 import { makeStyles, createStyles, Theme } from '@material-ui/core';
 import { useRouter } from 'next/router';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import ClearIcon from '@material-ui/icons/Clear';
-import SearchIcon from '@material-ui/icons/Search';
 import CarIcon from '@material-ui/icons/DriveEtaRounded';
 import DoneIcon from '@material-ui/icons/Done';
 import { useSelector } from 'react-redux';
@@ -15,7 +10,6 @@ import { IState } from '~/interfaces/IState';
 import { capitalize } from '~/utils';
 import { ICar } from '~/interfaces';
 import url from '~/services/url';
-import axios from 'axios';
 import SearchBar from '~/components/header/SearchBar';
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -32,41 +26,8 @@ const useStyles = makeStyles((theme: Theme) =>
       paddingLeft: theme.spacing(1),
       paddingRight: theme.spacing(1),
     },
-    autocomlete: {
-      width: '100%',
-    },
-    clearIcon: {
-      color: theme.palette.text.disabled,
-      fontSize: '90%',
-      cursor: 'pointer',
-    },
-    input: {
-      fontSize: '1.1rem',
-      /* color: theme.palette.text.secondary, */
-      '&::placeholder': {
-        fontSize: '1rem',
-      },
-    },
-    listbox: {
-      color: theme.palette.text.secondary,
-      fontSize: '1rem',
-    },
   })
 );
-const strip = (value: string) => value.replace(/[^a-zA-Z\s0-9]/g, '');
-
-function filterOut(text: string, cursor: number): [string, number] {
-  const beforeCursor = text.slice(0, cursor);
-  const afterCursor = text.slice(cursor, text.length);
-
-  const filterdBeforeCursor = strip(beforeCursor);
-  const filterAfterCursor = strip(afterCursor);
-
-  const newText = filterdBeforeCursor + filterAfterCursor;
-  const newCursor = filterdBeforeCursor.length;
-
-  return [newText, newCursor];
-}
 
 export default function SearchBox() {
   const classes = useStyles();
