2ee45469d5e3873cbc2337f6a98210d4c14a6041
mikohan <angara99@gmail.com>
2021-04-10 15:15:42 +0300

    Working on autocomplete and search

--- src/components/testRemove.tsx
+++ src/components/testRemove.tsx
@@ -6,7 +6,7 @@ import Autocomplete from '@material-ui/lab/Autocomplete';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import axios from 'axios';
 import { useRouter } from 'next/router';
-import { Button } from '@material-ui/core';
+import { Button, Grid } from '@material-ui/core';
 
 interface CountryType {
   name: string;
@@ -77,48 +77,55 @@ export default function Asynchronous() {
   return (
     <React.Fragment>
       <div>{inputValue}</div>
-      <Autocomplete
-        onInputChange={(e, value) => {
-          setInputValue(value);
-        }}
-        inputValue={inputValue}
-        id="autocomplete"
-        style={{ width: 300 }}
-        open={open}
-        onOpen={() => {
-          setOpen(true);
-        }}
-        onClose={() => {
-          setOpen(false);
-        }}
-        getOptionSelected={(option, value) => option.name === value.name}
-        getOptionLabel={(option) => option.name}
-        options={options}
-        filterOptions={(options, state) => options}
-        loading={loading}
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            label="Asynchronous"
-            variant="outlined"
-            InputProps={{
-              ...params.InputProps,
-              endAdornment: (
-                <React.Fragment>
-                  {loading ? (
-                    <CircularProgress color="inherit" size={20} />
-                  ) : null}
-                  {params.InputProps.endAdornment}
-                </React.Fragment>
-              ),
+      <Grid container>
+        <Grid item xs={8}>
+          <Autocomplete
+            freeSolo
+            onInputChange={(e, value) => {
+              setInputValue(value);
             }}
+            inputValue={inputValue}
+            id="autocomplete"
+            style={{ width: 300 }}
+            open={open}
+            onOpen={() => {
+              setOpen(true);
+            }}
+            onClose={() => {
+              setOpen(false);
+            }}
+            getOptionSelected={(option, value) => option.name === value.name}
+            getOptionLabel={(option) => option.name}
+            options={options}
+            filterOptions={(options, state) => options}
+            loading={loading}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Asynchronous"
+                variant="outlined"
+                InputProps={{
+                  ...params.InputProps,
+                  endAdornment: (
+                    <React.Fragment>
+                      {loading ? (
+                        <CircularProgress color="inherit" size={20} />
+                      ) : null}
+                      {params.InputProps.endAdornment}
+                    </React.Fragment>
+                  ),
+                }}
+              />
+            )}
           />
-        )}
-      />
-      <Button variant="outlined" onClick={handleSubmit}>
-        {' '}
-        Button
-      </Button>
+        </Grid>
+        <Grid item xs={4}>
+          <Button variant="outlined" onClick={handleSubmit}>
+            {' '}
+            Button
+          </Button>
+        </Grid>
+      </Grid>
     </React.Fragment>
   );
 }
