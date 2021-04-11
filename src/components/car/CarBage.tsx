6f04dfa3e0866e2d43dc788e231f4771027ca3f3
mikohan <angara99@gmail.com>
2021-04-11 16:56:10 +0300

    Working with car choiser on home page

--- src/components/car/CarBage.tsx
+++ src/components/car/CarBage.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import { Avatar, Chip } from '@material-ui/core';
+import { Avatar, Box } from '@material-ui/core';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import CarIcon from '@material-ui/icons/LocalShipping';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    popover: {
-      pointerEvents: 'none',
-    },
-    paper: {
-      padding: theme.spacing(1),
+    carBox: {
+      width: '95%',
+      paddingLeft: theme.spacing(1),
+      paddingRight: theme.spacing(1),
     },
   })
 );
@@ -23,17 +22,10 @@ export default function MouseOverPopover({ text, carImg }: IProps) {
   const classes = useStyles();
 
   return (
-    <div>
-      <Chip
-        avatar={
-          <Avatar src={carImg}>
-            <CarIcon />
-          </Avatar>
-        }
-        label={text}
-        color="default"
-        variant="outlined"
-      />
-    </div>
+    <Box className={classes.carBox}>
+      <Avatar src={carImg}>
+        <CarIcon />
+      </Avatar>
+    </Box>
   );
 }
