aab1701ad8dd9fc492b91fe6f40ee8d14a676980
mikohan <angara99@gmail.com>
2021-04-16 11:17:09 +0300

    Price box progress

--- src/components/product/productPage/PriceBox.tsx
+++ src/components/product/productPage/PriceBox.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import { Theme, makeStyles, createStyles } from '@material-ui/core/styles';
-import { Paper, Grid, Chip, Button, Typography } from '@material-ui/core';
+import { Paper, Grid, Chip, Button, Typography, Box } from '@material-ui/core';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     container: {
       height: theme.spacing(20),
+      border: '1px solid blue',
     },
     paperOne: {
       height: theme.spacing(20),
     },
-    firstRow: {
-      border: '1px solid blue',
+    firstRow: {},
+    price: {
+      padding: theme.spacing(2),
+    },
+    priceSide: {
+      padding: theme.spacing(2),
     },
   })
 );
@@ -20,15 +25,21 @@ const PriceBox = () => {
   const classes = useStyles();
   return (
     <div className={classes.container}>
-      <Paper>
-        <Grid container>
-          <Grid className={classes.firstRow} item xs={12}>
-            <Typography className={classes.paperOne} variant="body2">
-              Some content
-            </Typography>
-          </Grid>
+      <Grid container>
+        <Grid className={classes.firstRow} container item xs={12}>
+          <Box className={classes.price}>
+            <Typography variant="h4">R 5967</Typography>
+          </Box>
+          <Box className={classes.priceSide}>
+            <Typography variant="body2">Some content</Typography>
+          </Box>
         </Grid>
-      </Paper>
+        <Grid className={classes.secondRow} container item>
+          <Button variant="contained" color="primary">
+            Add to CART
+          </Button>
+        </Grid>
+      </Grid>
     </div>
   );
 };
