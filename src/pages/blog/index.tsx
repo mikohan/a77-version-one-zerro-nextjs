fe3d747037dbf342f1f6dfc8eebe8cf20c4c9d85
mikohan <angara99@gmail.com>
2021-04-23 17:25:22 +0300

    Start styling blog page

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import Link from 'next/link';
 import { IPost } from '~/interfaces';
 import { getPosts } from '~/endpoints/blogEndpoint';
-import { REVALIDATE } from '~/config';
+import { containerMaxWidth, REVALIDATE } from '~/config';
 import { GetStaticPropsContext } from 'next';
 import url from '~/services/url';
 import parse from 'html-react-parser';
 import { Theme, makeStyles, createStyles } from '@material-ui/core/styles';
 import { addMainUrlInPostImage } from '~/helpers';
 import AnimationPage from '~/components/common/AnimationPage';
-import { Grid, Box } from '@material-ui/core';
+import { Grid, Box, Container } from '@material-ui/core';
 import BlogHead from '~/components/heads/BlogHead';
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -30,31 +30,41 @@ interface IProps {
   posts: IPost[];
 }
 
+interface IPaperProps {
+  post: IPost;
+}
+
+function BlogPaper({ post }: IPaperProps) {
+  return (
+    <div>
+      <Link href={url.post(post.slug)}>
+        <div>{post.title}</div>
+      </Link>
+      <div>{parse(post.excerpt)}</div>
+    </div>
+  );
+}
+
 export default function Posts({ posts }: IProps) {
   const classes = useStyles();
   return (
     <React.Fragment>
       <BlogHead />
       <AnimationPage>
-        <Grid className={classes.main} container>
-          <Grid className={classes.some} container item xs={12} md={9}>
-            <div className={classes.container}>
-              {posts.map((post: IPost) => {
-                return (
-                  <div key={post.slug}>
-                    <Link href={url.post(post.slug)}>
-                      <div>{post.title}</div>
-                    </Link>
-                    <div>{parse(post.excerpt)}</div>
-                  </div>
-                );
-              })}
-            </div>
+        <Container maxWidth={containerMaxWidth}>
+          <Grid className={classes.main} container>
+            <Grid className={classes.some} container item xs={12} md={9}>
+              <div className={classes.container}>
+                {posts.map((post: IPost) => {
+                  return <BlogPaper key={post.slug} post={post} />;
+                })}
+              </div>
+            </Grid>
+            <Grid className={classes.sidePanel} item xs={12} md={3}>
+              side panel
+            </Grid>
           </Grid>
-          <Grid className={classes.sidePanel} item xs={12} md={3}>
-            side panel
-          </Grid>
-        </Grid>
+        </Container>
       </AnimationPage>
     </React.Fragment>
   );
