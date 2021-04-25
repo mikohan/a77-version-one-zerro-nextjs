dc483dbdd69f54f4a36fa45c58c6d390ea85f9ad
mikohan <angara99@gmail.com>
2021-04-25 16:31:38 +0300

    Make search page

---
+++ src/components/blog/PaginationSearch.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { makeStyles, createStyles } from '@material-ui/core/styles';
+import Pagination from '@material-ui/lab/Pagination';
+import { useRouter } from 'next/router';
+import url from '~/services/url';
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    root: {
+      '& > *': {
+        marginTop: theme.spacing(2),
+      },
+    },
+  })
+);
+interface IProps {
+  count: number;
+  curPage: number;
+  search: string;
+}
+
+export default function BasicPagination({
+  count,
+  search,
+  curPage = 1,
+}: IProps) {
+  const classes = useStyles();
+  const router = useRouter();
+  function handleChange(event: any, value: number) {
+    router.push(url.blogSearch(search, value));
+  }
+  return (
+    <div className={classes.root}>
+      <Pagination
+        count={count}
+        page={curPage}
+        color="secondary"
+        onChange={handleChange}
+      />
+    </div>
+  );
+}
