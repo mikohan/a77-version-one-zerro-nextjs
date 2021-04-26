26651e1541362159d5396343ba9dae849c4cd1b3
mikohan <angara99@gmail.com>
2021-04-26 12:50:26 +0300

    Working on single post page

---
+++ src/components/common/AvatarImage.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import Avatar from '@material-ui/core/Avatar';
+import Image from 'next/image';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+      '& > *': {
+        margin: theme.spacing(1),
+      },
+    },
+  })
+);
+
+interface IProps {
+  image: string;
+  props?: any;
+}
+
+export default function ImageAvatars({ image, ...props }: IProps) {
+  const classes = useStyles();
+  const src = image ? image : '/images/local/A100.png';
+
+  return (
+    <div className={classes.root}>
+      <Avatar alt="Remy Sharp" {...props}>
+        <Image src={src} width={100} height={100} />
+      </Avatar>
+    </div>
+  );
+}
