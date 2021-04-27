6300eb74a082648c0e7d44e6c0b17cb5bd541204
mikohan <angara99@gmail.com>
2021-04-27 17:13:23 +0300

    Working on car page

--- src/components/car/PopularMakesWidet.tsx
+++ src/components/car/PopularMakesWidet.tsx
@@ -5,15 +5,21 @@ import { IMake, IProduct } from '~/interfaces';
 import { capitalize } from '~/utils';
 import { imageServerUrl } from '~/config';
 import Image from 'next/image';
+import Link from 'next/link';
+import url from '~/services/url';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     container: {
       padding: theme.spacing(2),
     },
-    makeContainer: {
+    makeItem: {
+      borderTop: '1px solid red',
       display: 'flex',
       alignItems: 'center',
+    },
+    name: {
+      paddingLeft: theme.spacing(2),
     },
   })
 );
@@ -32,18 +38,24 @@ export default function LeftSidePopularWidget({ makes }: IProps): JSX.Element {
           <Typography variant="h6">Популярные Марки</Typography>
           {makes &&
             makes.map((make: IMake) => (
-              <Box key={make.slug} className={classes.makeContainer}>
-                <Image
-                  src={
-                    make && make.image
-                      ? `${imageServerUrl}${make.image}`
-                      : '/images/local/carsAvatar/generic.png'
-                  }
-                  width={75}
-                  height={60}
-                />
-                <Box>{capitalize(make.name)}</Box>
-              </Box>
+              <Link key={make.slug} href={url.make(make.slug)}>
+                <a>
+                  <Box className={classes.makeItem}>
+                    <Image
+                      src={
+                        make && make.image
+                          ? `${imageServerUrl}${make.image}`
+                          : '/images/local/carsAvatar/generic.png'
+                      }
+                      width={75}
+                      height={60}
+                    />
+                    <Typography className={classes.name} variant="body1">
+                      {capitalize(make.name)}
+                    </Typography>
+                  </Box>
+                </a>
+              </Link>
             ))}
         </Grid>
       </Grid>
