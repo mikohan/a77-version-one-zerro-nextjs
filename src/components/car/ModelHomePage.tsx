eb9ffde1d4395cd2be7008f88dbace6fd77c30db
mikohan <angara99@gmail.com>
2021-04-27 13:25:54 +0300

    Working on car page

--- src/components/car/ModelHomePage.tsx
+++ src/components/car/ModelHomePage.tsx
@@ -203,15 +203,12 @@ export default function ModelShopList(props: IProps) {
             <Typography variant="h6" className={classes.blockTitle}>
               Блог
             </Typography>
-            <Typography variant="body1">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
-              non odio reprehenderit illo facilis doloremque odit esse est nemo
-              alias assumenda eaque deserunt inventore quas hic sunt quae nam,
-              mollitia, dolorum, quia maiores eveniet? Unde enim laborum
-              veritatis possimus, odit vel maxime commodi, architecto recusandae
-              inventore ipsam, saepe sit provident reiciendis accusamus rerum
-              molestias voluptatem at dolor atque iure. Voluptas?
-            </Typography>
+            <Box className={classes.blogContainer}>
+              {postsByCar &&
+                postsByCar.map((post: IPost) => (
+                  <Box className={classes.blogItem}>{post.title}</Box>
+                ))}
+            </Box>
           </Paper>
         </Grid>
         <Grid item xs={12}>
