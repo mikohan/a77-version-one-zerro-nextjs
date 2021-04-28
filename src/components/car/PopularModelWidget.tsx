bad48e99949ca9132d9c4104c593d5642f3bf9ac
mikohan <angara99@gmail.com>
2021-04-28 15:58:41 +0300

    Start working with make page

---
+++ src/components/car/PopularModelWidget.tsx
@@ -0,0 +1,80 @@
+import React, { ReactNode } from 'react';
+import { Paper, Grid, Typography, Box } from '@material-ui/core';
+import { createStyles, makeStyles, Theme } from '@material-ui/core';
+import { ICar, IProduct } from '~/interfaces';
+import { capitalize } from '~/utils';
+import { imageServerUrl } from '~/config';
+import Image from 'next/image';
+import Link from 'next/link';
+import url from '~/services/url';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    container: {
+      paddingTop: theme.spacing(2),
+      paddingBottom: theme.spacing(2),
+    },
+    title: {
+      paddingLeft: theme.spacing(2),
+    },
+    makeItem: {
+      paddingTop: theme.spacing(1),
+      paddingBottom: theme.spacing(1),
+      paddingLeft: theme.spacing(2),
+      paddingRight: theme.spacing(2),
+      display: 'flex',
+      alignItems: 'center',
+      '&:hover': {
+        background: theme.palette.action.hover,
+      },
+    },
+    name: {
+      fontWeight: 500,
+      paddingLeft: theme.spacing(2),
+    },
+  })
+);
+
+interface IProps {
+  models: ICar[];
+}
+
+export default function LeftSidePopularWidget({ models }: IProps): JSX.Element {
+  const classes = useStyles();
+
+  return (
+    <Paper>
+      <Grid className={classes.container} container>
+        <Grid item xs={12}>
+          <Typography className={classes.title} variant="h6">
+            Популярные Модели
+          </Typography>
+          {models &&
+            models.map((model: ICar) => (
+              <Link
+                key={model.slug}
+                href={url.model(model.make.slug, model.slug)}
+              >
+                <a>
+                  <Box className={classes.makeItem}>
+                    <Image
+                      src={
+                        model && model.image
+                          ? `${imageServerUrl}${model.image}`
+                          : '/images/local/carsAvatar/generic.png'
+                      }
+                      width={75}
+                      height={60}
+                    />
+                    <Typography className={classes.name} variant="body1">
+                      {capitalize(model.model)}
+                    </Typography>
+                  </Box>
+                </a>
+              </Link>
+            ))}
+        </Grid>
+      </Grid>
+    </Paper>
+  );
+}
