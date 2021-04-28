e4856b560ef09303df1a968f8c2982ad927c2c7d
mikohan <angara99@gmail.com>
2021-04-28 15:31:52 +0300

    Start working with make page

--- src/components/car/ModelGridBlock.tsx
+++ src/components/car/ModelGridBlock.tsx
@@ -5,6 +5,7 @@ import { Box, Typography } from '@material-ui/core';
 import { Theme, makeStyles, createStyles } from '@material-ui/core/styles';
 import Link from 'next/link';
 import url from '~/services/url';
+import Image from 'next/image';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -14,6 +15,9 @@ const useStyles = makeStyles((theme: Theme) =>
       flexWrap: 'wrap',
     },
     item: {
+      display: 'flex',
+      minWidth: theme.spacing(20),
+      alignItems: 'center',
       border: '1px solid blue',
     },
   })
@@ -34,6 +38,7 @@ export default function ModelBlockGrid(props: ICarProps) {
           models.map((model: ICar) => (
             <Link href={url.model(make.slug, model.slug)} key={model.slug}>
               <a className={classes.item}>
+                <Image src={``} width={50} height={30} />
                 <Typography variant="h6">{model.model}</Typography>
               </a>
             </Link>
