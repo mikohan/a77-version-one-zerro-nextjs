2896d9043a5bbbac2aa8f30b152dbe74d5ea26f9
mikohan <angara99@gmail.com>
2021-05-09 10:01:29 +0300

    Working shopping cart

---
+++ src/store/cart/cartAction.ts
@@ -0,0 +1,80 @@
+// third-party
+import { Dispatch } from 'redux';
+// application
+import { ICartItemOption } from '~/store/cart/cartTypes';
+import { IProduct } from '~/interfaces/product';
+import {
+  CART_ADD_ITEM,
+  CART_REMOVE_ITEM,
+  CART_UPDATE_QUANTITIES,
+  CartAddItemAction,
+  CartItemQuantity,
+  CartRemoveItemAction,
+  CartUpdateQuantitiesAction,
+} from '~/store/cart/cartActionTypes';
+
+export function cartAddItemSuccess(
+  product: IProduct,
+  options: ICartItemOption[] = [],
+  quantity = 1
+): CartAddItemAction {
+  return {
+    type: CART_ADD_ITEM,
+    product,
+    options,
+    quantity,
+  };
+}
+
+export function cartRemoveItemSuccess(itemId: number): CartRemoveItemAction {
+  return {
+    type: CART_REMOVE_ITEM,
+    itemId,
+  };
+}
+
+export function cartUpdateQuantitiesSuccess(
+  quantities: CartItemQuantity[]
+): CartUpdateQuantitiesAction {
+  return {
+    type: CART_UPDATE_QUANTITIES,
+    quantities,
+  };
+}
+
+export function cartAddItem(
+  product: IProduct,
+  options: ICartItemOption[] = [],
+  quantity = 1
+): any {
+  // sending request to server, timeout is used as a stub
+  return (dispatch: Dispatch) =>
+    new Promise((resolve: any) => {
+      setTimeout(() => {
+        dispatch(cartAddItemSuccess(product, options, quantity));
+        resolve();
+      }, 250);
+    });
+}
+
+export function cartRemoveItem(itemId: number): any {
+  // sending request to server, timeout is used as a stub
+  return (dispatch: Dispatch) =>
+    new Promise((resolve: any) => {
+      setTimeout(() => {
+        dispatch(cartRemoveItemSuccess(itemId));
+        resolve();
+      }, 250);
+    });
+}
+
+export function cartUpdateQuantities(quantities: CartItemQuantity[]): any {
+  // sending request to server, timeout is used as a stub
+  return (dispatch: Dispatch) =>
+    new Promise((resolve: any) => {
+      setTimeout(() => {
+        dispatch(cartUpdateQuantitiesSuccess(quantities));
+        resolve();
+      }, 250);
+    });
+}
--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -3,9 +3,11 @@ import { combineReducers } from 'redux';
 import { changeCarModelReducer } from './changeCarModelReducer';
 import { UIReducer } from '~/store/ui/UIReducer';
 import { shopReducer } from '~/store/shop/shopReducer';
+import { cartReducer } from '~/store/cart/cartReducer';
 
 export default combineReducers({
   shop: changeCarModelReducer,
   shopNew: shopReducer,
   uiState: UIReducer,
+  cart: cartReducer,
 });
