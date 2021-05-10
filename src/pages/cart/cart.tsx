0f7195e2211f2b8fe535a657eeaa8fe603144b12
mikohan <angara99@gmail.com>
2021-05-10 12:06:01 +0300

    Working on mini cart

--- src/pages/cart/cart.tsx
+++ src/pages/cart/cart.tsx
@@ -116,6 +116,141 @@ export default function Cart() {
     dispatch(cartRemoveItemSuccess(itemId));
   }
 
+  function BigPaper() {
+    return (
+      <Paper>
+        <TableContainer>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>
+                  <Typography variant="body1">Фото</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography variant="body1">Продукт</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography variant="body1">Цена</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography variant="body1">Количество</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography variant="body1">Итого</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography variant="body1"></Typography>
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {cart.items.map((item: ICartItem) => {
+                const img =
+                  item.product.images && item.product.images.length
+                    ? `${imageServerUrl}${
+                        item.product.images[0].img150 as string
+                      }`
+                    : '/images/local/defaultParts245.jpg';
+                return (
+                  <TableRow key={item.id}>
+                    <TableCell>
+                      <Image src={img} width={120} height={100} />
+                    </TableCell>
+                    <TableCell>
+                      <Typography className={classes.cart} variant="body1">
+                        {item.product.name}{' '}
+                      </Typography>
+                    </TableCell>
+                    <TableCell>
+                      <Typography className={classes.cart} variant="body1">
+                        &#8381; {item.product.stocks[0].price}
+                      </Typography>
+                    </TableCell>
+                    <TableCell>
+                      <TextField
+                        defaultValue={item.quantity}
+                        variant="filled"
+                        type="number"
+                        size="small"
+                        InputProps={{
+                          inputProps: { min: 1, max: 20 },
+                        }}
+                        onChange={(
+                          event: React.ChangeEvent<HTMLInputElement>
+                        ) => handleQuantity(event, item)}
+                      ></TextField>
+                    </TableCell>
+                    <TableCell>
+                      <Typography className={classes.cart} variant="body1">
+                        &#8381; {item.total}
+                      </Typography>
+                    </TableCell>
+                    <TableCell align="right">
+                      <DeleteForeverIcon
+                        className={classes.deleteIcon}
+                        onClick={() => handleRemoveItem(item.id)}
+                      />
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Paper>
+    );
+  }
+
+  function SmallPaper() {
+    return (
+      <Paper>
+        <React.Fragment>
+          <TableContainer>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>
+                    <Typography variant="h6">Сумма Корзины</Typography>
+                  </TableCell>
+                  <TableCell></TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                <TableRow>
+                  <TableCell>
+                    <Typography className={classes.cart} variant="body1">
+                      Доставка
+                    </Typography>
+                  </TableCell>
+                  <TableCell>
+                    <Typography variant="body1">Звоните!</Typography>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>
+                    <Typography className={classes.cart} variant="body1">
+                      К Оплате
+                    </Typography>
+                  </TableCell>
+                  <TableCell>
+                    <Typography variant="body1">
+                      &#8381; {cart.total}
+                    </Typography>
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <Box className={classes.orderButton}>
+            <Button variant="contained" color="primary">
+              Оформить заказ
+            </Button>
+          </Box>
+        </React.Fragment>
+      </Paper>
+    );
+  }
+
   return (
     <React.Fragment>
       <AnimationPage>
@@ -127,158 +262,14 @@ export default function Cart() {
             {cart.items.length ? (
               <Grid container item xs={12}>
                 <Grid className={classes.gridColumn} item xs={12} md={8}>
-                  <Paper>
-                    <TableContainer>
-                      <Table>
-                        <TableHead>
-                          <TableRow>
-                            <TableCell>
-                              <Typography variant="body1">Фото</Typography>
-                            </TableCell>
-                            <TableCell>
-                              <Typography variant="body1">Продукт</Typography>
-                            </TableCell>
-                            <TableCell>
-                              <Typography variant="body1">Цена</Typography>
-                            </TableCell>
-                            <TableCell>
-                              <Typography variant="body1">
-                                Количество
-                              </Typography>
-                            </TableCell>
-                            <TableCell>
-                              <Typography variant="body1">Итого</Typography>
-                            </TableCell>
-                            <TableCell>
-                              <Typography variant="body1"></Typography>
-                            </TableCell>
-                          </TableRow>
-                        </TableHead>
-                        <TableBody>
-                          {cart.items.map((item: ICartItem) => {
-                            const img =
-                              item.product.images && item.product.images.length
-                                ? `${imageServerUrl}${
-                                    item.product.images[0].img150 as string
-                                  }`
-                                : '/images/local/defaultParts245.jpg';
-                            return (
-                              <TableRow key={item.id}>
-                                <TableCell>
-                                  <Image src={img} width={120} height={100} />
-                                </TableCell>
-                                <TableCell>
-                                  <Typography
-                                    className={classes.cart}
-                                    variant="body1"
-                                  >
-                                    {item.product.name}{' '}
-                                  </Typography>
-                                </TableCell>
-                                <TableCell>
-                                  <Typography
-                                    className={classes.cart}
-                                    variant="body1"
-                                  >
-                                    &#8381; {item.product.stocks[0].price}
-                                  </Typography>
-                                </TableCell>
-                                <TableCell>
-                                  <TextField
-                                    defaultValue={item.quantity}
-                                    variant="filled"
-                                    type="number"
-                                    size="small"
-                                    InputProps={{
-                                      inputProps: { min: 1, max: 20 },
-                                    }}
-                                    onChange={(
-                                      event: React.ChangeEvent<HTMLInputElement>
-                                    ) => handleQuantity(event, item)}
-                                  ></TextField>
-                                </TableCell>
-                                <TableCell>
-                                  <Typography
-                                    className={classes.cart}
-                                    variant="body1"
-                                  >
-                                    &#8381; {item.total}
-                                  </Typography>
-                                </TableCell>
-                                <TableCell align="right">
-                                  <DeleteForeverIcon
-                                    className={classes.deleteIcon}
-                                    onClick={() => handleRemoveItem(item.id)}
-                                  />
-                                </TableCell>
-                              </TableRow>
-                            );
-                          })}
-                        </TableBody>
-                      </Table>
-                    </TableContainer>
-                  </Paper>
+                  {/* <BigPaper /> */}
                 </Grid>
                 <Grid className={classes.gridColumn} item xs={12} md={4}>
-                  <Paper>
-                    <React.Fragment>
-                      <TableContainer>
-                        <Table>
-                          <TableHead>
-                            <TableRow>
-                              <TableCell>
-                                <Typography variant="h6">
-                                  Сумма Корзины
-                                </Typography>
-                              </TableCell>
-                              <TableCell></TableCell>
-                            </TableRow>
-                          </TableHead>
-                          <TableBody>
-                            <TableRow>
-                              <TableCell>
-                                <Typography
-                                  className={classes.cart}
-                                  variant="body1"
-                                >
-                                  Доставка
-                                </Typography>
-                              </TableCell>
-                              <TableCell>
-                                <Typography variant="body1">
-                                  Звоните!
-                                </Typography>
-                              </TableCell>
-                            </TableRow>
-                            <TableRow>
-                              <TableCell>
-                                <Typography
-                                  className={classes.cart}
-                                  variant="body1"
-                                >
-                                  К Оплате
-                                </Typography>
-                              </TableCell>
-                              <TableCell>
-                                <Typography variant="body1">
-                                  &#8381; {cart.total}
-                                </Typography>
-                              </TableCell>
-                            </TableRow>
-                          </TableBody>
-                        </Table>
-                      </TableContainer>
-                      <Box className={classes.orderButton}>
-                        <Button variant="contained" color="primary">
-                          Оформить заказ
-                        </Button>
-                      </Box>
-                    </React.Fragment>
-                  </Paper>
+                  {/* <SmallPaper /> */}
                 </Grid>
               </Grid>
             ) : (
-              <Box className={classes.empty}>
+              <Grid container item className={classes.empty}>
                 <Typography className={classes.emptyGoHomeText} variant="h2">
                   Корзина Пуста
                 </Typography>
@@ -289,7 +280,7 @@ export default function Cart() {
                 >
                   Вернуться на Главную
                 </Button>
-              </Box>
+              </Grid>
             )}
           </Grid>
         </div>
