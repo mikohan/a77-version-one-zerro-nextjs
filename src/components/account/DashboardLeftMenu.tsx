557355b19125246a6eb98bb2f3109547f8eb2f61
mikohan <angara99@gmail.com>
2021-05-14 19:11:13 +0300

    Address editing done

--- src/components/account/DashboardLeftMenu.tsx
+++ src/components/account/DashboardLeftMenu.tsx
@@ -4,7 +4,7 @@ import List from '@material-ui/core/List';
 import ListItem, { ListItemProps } from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Divider from '@material-ui/core/Divider';
-import { Typography, Paper } from '@material-ui/core';
+import { Typography, Paper, MenuItem } from '@material-ui/core';
 import { signOut } from 'next-auth/client';
 import { useRouter } from 'next/router';
 import url from '~/services/url';
@@ -35,19 +35,23 @@ export default function SimpleList() {
   function handleProfile() {
     router.push(url.account.profile());
   }
+  function handleAddresses() {
+    router.push(url.account.addresses());
+  }
 
   return (
     <Paper className={classes.root}>
       <Typography className={classes.title} variant="h6">
         Навигация
       </Typography>
-      <List component="nav" aria-label="main mailbox folders">
-        <ListItem button>
+      <List>
+        <MenuItem>
           <ListItemText primary="Гараж" onClick={handleGarage} />
-        </ListItem>
-        <ListItem button>
+        </MenuItem>
+        <MenuItem>
           <ListItemText primary="Профиль" onClick={handleProfile} />
-        </ListItem>
+        </MenuItem>
+        <MenuItem onClick={handleAddresses}>Addresses</MenuItem>
       </List>
       <Divider />
       <List component="nav" aria-label="secondary mailbox folders">
