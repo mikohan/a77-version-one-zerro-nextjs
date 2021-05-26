9b7911f4b64a23a805970dfb48a2fd6996a30d99
mikohan <angara99@gmail.com>
2021-05-26 14:01:11 +0300

    Profile edit page is working

--- src/pages/account/profile.tsx
+++ src/pages/account/profile.tsx
@@ -21,9 +21,9 @@ import { useRouter } from 'next/router';
 import { IUser } from '~/interfaces';
 import { getUserCookie } from '~/services/getUserCookie';
 import NoLoggedIn from '~/components/account/NotLoggedIn';
-import Image from 'next/image';
 import axios from 'axios';
 import { backServerUrlRest } from '~/config';
+import Skeleton from '@material-ui/lab/Skeleton';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -66,6 +66,21 @@ const useStyles = makeStyles((theme: Theme) =>
       height: theme.spacing(15),
       marginLeft: theme.spacing(5),
     },
+    skelPaper: {
+      padding: theme.spacing(5),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    skelItem: {
+      marginBottom: theme.spacing(3),
+    },
+    skelBox: {
+      width: '100%',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+    },
   })
 );
 // This is the recommended way for Next.js 9.3 or newer
@@ -75,19 +90,14 @@ interface IProps {
 }
 export default function Dashboard({ user, access }: IProps) {
   const classes = useStyles();
-  const router = useRouter();
-  function goProfile() {
-    router.push(url.accont.profile());
-  }
-  const addresses = user.address_user;
 
   const [userName, setUserName] = useState(user.username);
   const [firstName, setFirstName] = useState(user.first_name || '');
   const [lastName, setLastName] = useState(user.last_name || '');
-  const [email, setEmail] = useState(user.email);
   const [phone, setPhone] = useState(user.phone || '');
   const [avatar, setAvatar] = useState(user.image);
   const [avatarUpload, setAvatarUpload] = useState<{ file: File } | null>(null);
+  const [loading, setLoading] = useState(false);
 
   function handleUserName(event: React.ChangeEvent<HTMLInputElement>) {
     setUserName(event.target.value);
@@ -97,9 +107,6 @@ export default function Dashboard({ user, access }: IProps) {
   }
   function handleLastName(event: React.ChangeEvent<HTMLInputElement>) {
     setLastName(event.target.value);
-  }
-  function handleEmail(event: React.ChangeEvent<HTMLInputElement>) {
-    setEmail(event.target.value);
   }
   function handlePhone(event: React.ChangeEvent<HTMLInputElement>) {
     setPhone(event.target.value);
@@ -118,7 +125,6 @@ export default function Dashboard({ user, access }: IProps) {
     formData.append('username', userName);
     formData.append('first_name', firstName as string);
     formData.append('last_name', lastName as string);
-    formData.append('email', email);
     formData.append('phone', phone as string);
     const config = {
       headers: {
@@ -128,12 +134,64 @@ export default function Dashboard({ user, access }: IProps) {
     };
 
     const userUrl = `${backServerUrlRest}/api/user/users/${user.id}/`;
-    const userBack = await axios.put(userUrl, formData, config);
-    setUserName(userBack.data.username);
-    setFirstName(userBack.data.first_name);
-    setLastName(userBack.data.last_name);
-    setPhone(userBack.data.phone);
-    setAvatar(userBack.data.image);
+    setLoading(true);
+    try {
+      const userBack = await axios.put(userUrl, formData, config);
+      setUserName(userBack.data.username);
+      setFirstName(userBack.data.first_name);
+      setLastName(userBack.data.last_name);
+      setPhone(userBack.data.phone);
+      setAvatar(userBack.data.image);
+      setLoading(false);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  function ProfileSkeleton() {
+    return (
+      <React.Fragment>
+        <AnimationPage id="skeleton">
+          <Paper className={classes.skelPaper}>
+            <Box>
+              <Skeleton
+                className={classes.skelItem}
+                variant="rect"
+                width={400}
+                height={40}
+              />
+              <Skeleton
+                className={classes.skelItem}
+                variant="rect"
+                width={400}
+                height={40}
+              />
+              <Skeleton
+                className={classes.skelItem}
+                variant="rect"
+                width={400}
+                height={40}
+              />
+              <Skeleton
+                className={classes.skelItem}
+                variant="rect"
+                width={400}
+                height={40}
+              />
+              <Box className={classes.skelBox}>
+                <Skeleton
+                  className={classes.skelItem}
+                  variant="rect"
+                  width={100}
+                  height={40}
+                />
+
+                <Skeleton variant="circle" width={150} height={150} />
+              </Box>
+            </Box>
+          </Paper>
+        </AnimationPage>
+      </React.Fragment>
+    );
   }
 
   if (access) {
@@ -154,80 +212,89 @@ export default function Dashboard({ user, access }: IProps) {
                 <Grid container>
                   <Grid item container xs={12}>
                     <Grid item xs={12}>
-                      <Paper className={classes.profilePaper}>
-                        <Typography className={classes.pageTitle} variant="h6">
-                          Изменить Профиль
-                        </Typography>
-                        <Box className={classes.form}>
-                          <Box className={classes.fieldsBox}>
-                            <TextField
-                              id="userName"
-                              label="Ник"
-                              variant="filled"
-                              fullWidth
-                              size="small"
-                              name="username"
-                              defaultValue={userName}
-                              onChange={handleUserName}
-                            />
-                            <TextField
-                              id="firstName"
-                              label="Имя"
-                              variant="filled"
-                              fullWidth
-                              size="small"
-                              name="firstName"
-                              defaultValue={firstName}
-                              onChange={handleFirstName}
-                            />
-                            <TextField
-                              id="lastName"
-                              label="Фамилия"
-                              variant="filled"
-                              fullWidth
-                              size="small"
-                              name="lastName"
-                              defaultValue={lastName}
-                              onChange={handleLastName}
-                            />
-                            <TextField
-                              id="phone"
-                              label="Телефон"
-                              variant="filled"
-                              fullWidth
-                              size="small"
-                              name="phone"
-                              defaultValue={phone}
-                              onChange={handlePhone}
-                            />
-                            <Box className={classes.image}>
-                              <Button variant="contained" component="label">
-                                Загрузить Аватар
-                                <input
-                                  onChange={handleAvatar}
-                                  type="file"
-                                  hidden
-                                />
-                              </Button>
-                              <Avatar
-                                className={classes.avatar}
-                                src={
-                                  avatar
-                                    ? avatar
-                                    : '/images/local/default-avatar.jpg'
-                                }
-                              ></Avatar>
-                            </Box>
-                            <Button
-                              variant="contained"
-                              color="primary"
-                              onClick={handleSubmit}
+                      {!loading ? (
+                        <AnimationPage id="profle">
+                          <Paper className={classes.profilePaper}>
+                            <Typography
+                              className={classes.pageTitle}
+                              variant="h6"
                             >
-                              Сохранить
-                            </Button>
-                          </Box>
-                        </Box>
-                      </Paper>
+                              Изменить Профиль
+                            </Typography>
+                            <Box className={classes.form}>
+                              <Box className={classes.fieldsBox}>
+                                <TextField
+                                  id="userName"
+                                  label="Ник"
+                                  variant="filled"
+                                  fullWidth
+                                  size="small"
+                                  name="username"
+                                  defaultValue={userName}
+                                  onChange={handleUserName}
+                                />
+                                <TextField
+                                  id="firstName"
+                                  label="Имя"
+                                  variant="filled"
+                                  fullWidth
+                                  size="small"
+                                  name="firstName"
+                                  defaultValue={firstName}
+                                  onChange={handleFirstName}
+                                />
+                                <TextField
+                                  id="lastName"
+                                  label="Фамилия"
+                                  variant="filled"
+                                  fullWidth
+                                  size="small"
+                                  name="lastName"
+                                  defaultValue={lastName}
+                                  onChange={handleLastName}
+                                />
+                                <TextField
+                                  id="phone"
+                                  label="Телефон"
+                                  variant="filled"
+                                  fullWidth
+                                  size="small"
+                                  name="phone"
+                                  defaultValue={phone}
+                                  onChange={handlePhone}
+                                />
+                                <Box className={classes.image}>
+                                  <Button variant="contained" component="label">
+                                    Загрузить Аватар
+                                    <input
+                                      onChange={handleAvatar}
+                                      type="file"
+                                      hidden
+                                    />
+                                  </Button>
+                                  <Avatar
+                                    className={classes.avatar}
+                                    src={
+                                      avatar
+                                        ? avatar
+                                        : '/images/local/default-avatar.jpg'
+                                    }
+                                  ></Avatar>
+                                </Box>
+                                <Button
+                                  variant="contained"
+                                  color="primary"
+                                  onClick={handleSubmit}
+                                >
+                                  Сохранить
+                                </Button>
+                              </Box>
+                            </Box>
+                          </Paper>
+                        </AnimationPage>
+                      ) : (
+                        <ProfileSkeleton />
+                      )}
                     </Grid>
                   </Grid>
                 </Grid>
