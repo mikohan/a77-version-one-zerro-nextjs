27805c4fc7e784b62369868824d0b649676c4b96
mikohan <angara99@gmail.com>
2021-05-31 18:57:12 +0300

    Fixed name of productCardSmall

--- src/components/blog/ProductGrid.tsx
+++ src/components/blog/ProductGrid.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import { IProduct } from '~/interfaces/product';
 import { Grid } from '@material-ui/core';
-import ProductCard from './ProductCArdSmall';
+import ProductCard from './ProductCardSmall';
 
 interface IProps {
   products: IProduct[];
