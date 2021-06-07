9a64415bb7349e1341763c47e8e91f5679d7ead0
mikohan <angara99@gmail.com>
2021-06-07 17:18:26 +0300

    Comm

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,74 +1,129 @@
 import React from 'react';
 import Head from 'next/head';
 import AnimationPage from '~/components/common/AnimationPage';
-import { footerData, imageServerUrl, SITE_DOMAIN_FULL } from '~/config';
-import { Button, Box, Grid, Typography, Container } from '@material-ui/core';
+import { footerData, SITE_DOMAIN_FULL } from '~/config';
+import { Paper, Grid, Typography } from '@material-ui/core';
 import { Theme, makeStyles, createStyles } from '@material-ui/core/styles';
-import { getPage } from '~/endpoints/blogEndpoint';
-import { IPage } from '~/interfaces';
-import { getLatestProducts } from '~/endpoints/productEndpoint';
-import { translateProducts } from '~/utils';
-import { IProduct } from '~/interfaces';
+import BreadCrumbs from '~/components/common/BreadCrumbs';
+import url from '~/services/url';
+//Comment to gh
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    main: {
+    container: {
+      paddingBottom: theme.spacing(5),
+      margin: '0 auto',
+      [theme.breakpoints.down('lg')]: {
+        maxWidth: '80%',
+      },
+      [theme.breakpoints.up('xl')]: {
+        maxWidth: '70%',
+      },
+    },
+    breads: {
+      paddingTop: theme.spacing(2),
+    },
+    title: {
+      paddingTop: theme.spacing(3),
+      paddingBottom: theme.spacing(2),
+    },
+    paper: {
+      display: 'flex',
+      flexDirection: 'column',
+      padding: theme.spacing(5),
+      height: '100%',
+      width: '100%',
+    },
+    mapGrid: {
+      paddingTop: theme.spacing(2),
+    },
+    mapPaper: {
       padding: theme.spacing(2),
     },
-    html: {
-      '& p, & span': {
-        fontSize: '1rem',
+    horisontalBox: {
+      display: 'flex',
+      alignItems: 'center',
+    },
+    row: {
+      paddingBottom: theme.spacing(2),
+    },
+    subtitle: {
+      paddingBottom: theme.spacing(2),
+    },
+    p: {
+      paddingBottom: theme.spacing(2),
+      '&>span': {
+        fontWeight: 700,
       },
-      '& h2, & h3, & h4': {
-        fontSize: '1.4rem',
-      },
-      '& li, & ol': {
-        fontSize: '1.1rem',
-        fontWeight: 'bold',
-      },
+    },
+    payment: {
+      display: 'flex',
+      flexDirection: 'column',
+    },
+    item: {
+      paddingTop: theme.spacing(1),
+      paddingBottom: theme.spacing(1),
     },
   })
 );
 
-interface IProps {
-  page: IPage;
-  products: IProduct[];
-}
-
-export default function About({ page, products }: IProps) {
+export default function About() {
   const classes = useStyles();
+  const breadCrumbs = [
+    { name: 'Ангара77', path: '/' },
+    { name: 'Оплата', path: url.payment() },
+  ];
   return (
     <React.Fragment>
       <AboutHead />
       <AnimationPage>
-        <Container maxWidth="lg">
-          <Grid className={classes.main} container>
-            <Grid item xs={12}>
-              <Typography variant="h1">{page.title}</Typography>
-            </Grid>
-            <Grid item xs={12}></Grid>
+        <Grid className={classes.container} container>
+          <Grid className={classes.breads} item xs={12}>
+            <BreadCrumbs breadCrumbs={breadCrumbs} />
           </Grid>
-        </Container>
+          <Grid item xs={12}>
+            <Typography className={classes.title} variant="h1">
+              Способы оплаты
+            </Typography>
+          </Grid>
+          <Grid className={classes.row} container item xs={12}>
+            <Paper className={classes.paper}>
+              <Typography className={classes.subtitle} variant="h6">
+                Something about company
+              </Typography>
+              <Grid className={classes.payment} item xs={12}></Grid>
+            </Paper>
+          </Grid>
+          <Grid className={classes.row} container item xs={12}>
+            <Paper className={classes.paper}>
+              <Typography className={classes.subtitle} variant="h6">
+                Как оплатить товар из регионов?
+              </Typography>
+              <Grid className={classes.payment} item xs={12}>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum
+                optio nemo, praesentium quo suscipit corporis quod tenetur
+                sapiente dolorem minus quae natus itaque aliquid tempora
+                quibusdam vero nam soluta inventore ad molestiae at sit nisi
+                commodi. Praesentium autem quae pariatur consequatur omnis vero
+                eveniet soluta veniam quos enim sed officia, quo debitis nihil
+                tempora dolorum quas quidem laboriosam non libero placeat beatae
+                similique quam. Sapiente porro odit dolorem sequi debitis,
+                delectus fugit illo ullam facere aut ad maiores iste. Esse
+                libero aliquam explicabo at fuga recusandae repudiandae nesciunt
+                et nobis. Veniam officia quos vitae labore aperiam voluptates
+                optio temporibus molestiae!
+              </Grid>
+            </Paper>
+          </Grid>
+        </Grid>
       </AnimationPage>
     </React.Fragment>
   );
 }
-export const getStaticProps: any = async (context: any) => {
-  const page = await getPage('politika-konfidentsialnosti');
-  const prods = await getLatestProducts(20);
-  const products = translateProducts(prods.hits.hits);
-
-  return {
-    props: {
-      page,
-      products,
-    },
-  };
-};
 
 const AboutHead = () => (
   <Head>
-    <title key="title">About US - History & Team | Angara Parts</title>
+    <title key="title">О Компании | Angara Parts</title>
     <meta
       key="description"
       name="description"
