e0ed5d9c082d24b624f5ece64e855905864cbfc5
mikohan <angara99@gmail.com>
2021-06-08 11:57:43 +0300

    json ld making

--- src/components/heads/ProductPageHead.tsx
+++ src/components/heads/ProductPageHead.tsx
@@ -2,15 +2,20 @@
 import Head from 'next/head';
 import { footerData, SITE_DOMAIN_FULL } from '~/config';
 import { capitalize } from '~/utils';
-import { IProduct } from '~/interfaces';
+import { ICar, IProduct } from '~/interfaces';
+import url from '~/services/url';
 
 interface IProps {
   product: IProduct;
 }
 
 export default function ProductPageHead({ product }: IProps) {
+  console.log(product.model.map((model: ICar) => model.model));
   const mk = capitalize(product.name);
-  console.log(product);
+  const today = new Date();
+  const date = `${today.getFullYear()}-${
+    today.getMonth() + 1
+  }-${today.getDay()}`;
   return (
     <Head>
       <title key="title">Запчасти {mk} в наличии | Angara 77 Parts</title>
@@ -19,30 +24,30 @@ export default function ProductPageHead({ product }: IProps) {
         name="description"
         content={`Запчасти для ${mk} с доставкой и со склада в интерет магазине АНГАРА77 | Звоните ${footerData.SHOP_PHONE}!`}
       />
-      <meta
-        key="og:title"
-        property="og:title"
-        content="Get your car in perfect health | Angara Parts | About Us"
-      />
-      <meta
-        key="og:url"
-        property="og:url"
-        content={`${SITE_DOMAIN_FULL}/about`}
-      />
-      <meta key="og:image" property="og:image" content="/favicon.png" />
-      <meta key="og:image:type" property="og:image:type" content="image/png" />
-      <meta key="og:image:width" property="og:image:width" content="1200" />
-      <meta key="og:image:hight" property="og:image:hight" content="630" />
+      {/* <meta */}
+      {/*   key="og:title" */}
+      {/*   property="og:title" */}
+      {/*   content="Get your car in perfect health | Angara Parts | About Us" */}
+      {/* /> */}
+      {/* <meta */}
+      {/*   key="og:url" */}
+      {/*   property="og:url" */}
+      {/*   content={`${SITE_DOMAIN_FULL}/about`} */}
+      {/* /> */}
+      {/* <meta key="og:image" property="og:image" content="/favicon.png" /> */}
+      {/* <meta key="og:image:type" property="og:image:type" content="image/png" /> */}
+      {/* <meta key="og:image:width" property="og:image:width" content="1200" /> */}
+      {/* <meta key="og:image:hight" property="og:image:hight" content="630" /> */}
 
-      <meta
-        key="og:image:alt"
-        property="og:image:alt"
-        content="Angara 77 logo"
-      />
+      {/* <meta */}
+      {/*   key="og:image:alt" */}
+      {/*   property="og:image:alt" */}
+      {/*   content="Angara 77 logo" */}
+      {/* /> */}
       <link
         rel="canonical"
         key="canonical"
-        href={`${SITE_DOMAIN_FULL}/about`}
+        href={`${SITE_DOMAIN_FULL}${url.product(product.slug)}`}
       />
       <script
         type="application/ld+json"
@@ -50,43 +55,34 @@ export default function ProductPageHead({ product }: IProps) {
           __html: JSON.stringify({
             '@context': 'https://schema.org/',
             '@type': 'Product',
-            name: 'Example product',
-            image: [
-              'https://example.com/photos/1x1/photo.jpg',
-              'https://example.com/photos/4x3/photo.jpg',
-              'https://example.com/photos/16x9/photo.jpg',
-            ],
-            description:
-              'Example product is the best example product out there. Make sure to get the one and only -- the original.',
-            sku: '0374984678',
-            mpn: '738930',
+            name: `${mk}`,
+            image: product.images.map((img: any) => img.img245),
+            description: `Запчасти для ${mk} с доставкой и со склада в интерет магазине АНГАРА77 | Звоните ${footerData.SHOP_PHONE}!`,
+            sku: product.sku,
+            mpn: product.catNumber,
             brand: {
               '@type': 'Brand',
-              name: 'Example',
+              name: product.brand.name,
             },
             review: {
               '@type': 'Review',
               reviewRating: {
                 '@type': 'Rating',
-                ratingValue: '4',
+                ratingValue: product.rating,
                 bestRating: '5',
-              },
-              author: {
-                '@type': 'Person',
-                name: 'Hank Williams',
               },
             },
             aggregateRating: {
               '@type': 'AggregateRating',
-              ratingValue: '4.7',
-              reviewCount: '1455',
+              ratingValue: product.rating,
+              reviewCount: product.ratingCount,
             },
             offers: {
               '@type': 'Offer',
               url: 'https://example.com/product',
-              priceCurrency: 'USD',
-              price: '49.99',
-              priceValidUntil: '2021-11-20',
+              priceCurrency: 'RUB',
+              price: product.stocks[0].price,
+              priceValidUntil: date,
               itemCondition: 'https://schema.org/NewCondition',
               availability: 'https://schema.org/InStock',
             },
