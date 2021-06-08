54825115537c0846d31fa7a28eb4a8330fb6da32
mikohan <angara99@gmail.com>
2021-06-08 16:57:04 +0300

    Done with warranty page

--- src/pages/delivery/index.tsx
+++ src/pages/delivery/index.tsx
@@ -182,28 +182,7 @@ const AboutHead = () => {
       <meta
         key="description"
         name="description"
-        content={`Angara 77 | ${footerData.SHOP_PHONE} Information about our
-          company and history of establishment. We are open our dors in 2001 first time`}
-      />
-      <meta
-        key="og:title"
-        property="og:title"
-        content="Get your car in perfect health | Angara Parts | About Us"
-      />
-      <meta
-        key="og:url"
-        property="og:url"
-        content={`${SITE_DOMAIN_FULL}/about`}
-      />
-      <meta key="og:image" property="og:image" content="/favicon.png" />
-      <meta key="og:image:type" property="og:image:type" content="image/png" />
-      <meta key="og:image:width" property="og:image:width" content="1200" />
-      <meta key="og:image:hight" property="og:image:hight" content="630" />
-
-      <meta
-        key="og:image:alt"
-        property="og:image:alt"
-        content="Angara 77 logo"
+        content={`Доставка компании Ангара Запчасти | ${footerData.SHOP_PHONE}. Информация о способах и стоимости доставки по Москве и в регионы`}
       />
       <link
         rel="canonical"
--- src/pages/warranty/index.tsx
+++ src/pages/warranty/index.tsx
@@ -4,7 +4,7 @@ import AnimationPage from '~/components/common/AnimationPage';
 import { footerData, SITE_DOMAIN_FULL } from '~/config';
 import { Box, Paper, Grid, Typography, NoSsr } from '@material-ui/core';
 import { Theme, makeStyles, createStyles } from '@material-ui/core/styles';
-import { IPage } from '~/interfaces';
+import { IPage, IBread } from '~/interfaces';
 import { COMPANY_INFORMATION } from '~/config';
 import url from '~/services/url';
 import GoogleMap from '~/components/companyPages/GoogleMap';
@@ -62,13 +62,13 @@ interface IProps {
   page: IPage;
   google_key: string;
 }
+const breadCrumbs = [
+  { name: 'Ангара77', path: '/' },
+  { name: 'Гарантии', path: url.warranty() },
+];
 
 export default function About({ google_key }: IProps) {
   const classes = useStyles();
-  const breadCrumbs = [
-    { name: 'Ангара77', path: '/' },
-    { name: 'Гарантии', path: url.warranty() },
-  ];
   return (
     <React.Fragment>
       <AboutHead />
@@ -174,31 +174,36 @@ export const getStaticProps: any = async (context: any) => {
   };
 };
 
-const AboutHead = () => (
-  <Head>
-    <title key="title">Гарантия | Angara Parts</title>
-    <meta
-      key="description"
-      name="description"
-      content={`Angara 77 | ${footerData.SHOP_PHONE} Information about our
-          company and history of establishment. We are open our dors in 2001 first time`}
-    />
-    <meta
-      key="og:title"
-      property="og:title"
-      content="Get your car in perfect health | Angara Parts | About Us"
-    />
-    <meta
-      key="og:url"
-      property="og:url"
-      content={`${SITE_DOMAIN_FULL}/about`}
-    />
-    <meta key="og:image" property="og:image" content="/favicon.png" />
-    <meta key="og:image:type" property="og:image:type" content="image/png" />
-    <meta key="og:image:width" property="og:image:width" content="1200" />
-    <meta key="og:image:hight" property="og:image:hight" content="630" />
-
-    <meta key="og:image:alt" property="og:image:alt" content="Angara 77 logo" />
-    <link rel="canonical" key="canonical" href={`${SITE_DOMAIN_FULL}/about`} />
-  </Head>
-);
+const AboutHead = () => {
+  const breadItems = breadCrumbs.map((bread: IBread, i: number) => ({
+    '@type': 'ListItem',
+    position: i + 1,
+    name: bread.name,
+    item: `${SITE_DOMAIN_FULL}${bread.path}`,
+  }));
+  return (
+    <Head>
+      <title key="title">Гарантия | Angara Parts</title>
+      <meta
+        key="description"
+        name="description"
+        content={`Ангара Запчасти | ${footerData.SHOP_PHONE}. Информация о Гарантии и условиях возврата.`}
+      />
+      <link
+        rel="canonical"
+        key="canonical"
+        href={`${SITE_DOMAIN_FULL}${url.warranty()}`}
+      />
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+            '@context': 'https://schema.org',
+            '@type': 'BreadcrumbList',
+            itemListElement: breadItems,
+          }),
+        }}
+      />
+    </Head>
+  );
+};
