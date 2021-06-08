f3eef902ff69623a7788d9d8d9c956c49fe9e0a0
mikohan <angara99@gmail.com>
2021-06-08 14:19:27 +0300

    Start categories

--- src/components/heads/CategoryHead.tsx
+++ src/components/heads/CategoryHead.tsx
@@ -24,27 +24,28 @@ export default function CarModelHead({ model, category }: IProps) {
         name="description"
         content={`${ct} на ${mk} ${md} В Наличии в Интернет Магазине Ангара 77. Доставка и Самовывоз в Москве | ${footerData.SHOP_PHONE} `}
       />
-      <meta
-        key="og:title"
-        property="og:title"
-        content={`{ct} на {mk} {md} В Наличии | Angara Parts`}
-      />
-      {/* needs to be redone*/}
-      <meta key="og:url" property="og:url" content={`${SITE_DOMAIN_FULL}/`} />
-      <meta key="og:image" property="og:image" content="/favicon.png" />
-      <meta key="og:image:type" property="og:image:type" content="image/png" />
-      <meta key="og:image:width" property="og:image:width" content="1200" />
-      <meta key="og:image:hight" property="og:image:hight" content="630" />
-
-      <meta
-        key="og:image:alt"
-        property="og:image:alt"
-        content="Angara 77 logo"
-      />
       <link
         rel="canonical"
         key="canonical"
         href={url.category(model.make.slug, model.slug, category.slug)}
+      />
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+            '@context': 'http://schema.org',
+            '@type': 'Organization',
+            name: 'Ангара Запчасти',
+            url: SITE_DOMAIN_FULL,
+            logo: `${SITE_DOMAIN_FULL}/images/local/logo.png`,
+            foundingDate: '2007',
+            potentialAction: {
+              '@type': 'SearchAction',
+              target: `${SITE_DOMAIN_FULL}/search?search={search_term_string}`,
+              'query-input': 'required name=search_term_string',
+            },
+          }),
+        }}
       />
     </Head>
   );
