b7a89e848747fe8356fa994fc463bbea1c63a288
mikohan <angara99@gmail.com>
2021-06-08 16:45:59 +0300

    Done with breads on contact page

--- src/pages/contacts/index.tsx
+++ src/pages/contacts/index.tsx
@@ -5,7 +5,7 @@ import { footerData, SITE_DOMAIN_FULL } from '~/config';
 import { Box, Paper, Grid, Typography } from '@material-ui/core';
 import { Theme, makeStyles, createStyles } from '@material-ui/core/styles';
 import { getPage } from '~/endpoints/blogEndpoint';
-import { IPage } from '~/interfaces';
+import { IPage, IBread } from '~/interfaces';
 import { COMPANY_INFORMATION } from '~/config';
 import Link from 'next/link';
 import url from '~/services/url';
@@ -79,13 +79,13 @@ interface IProps {
   page: IPage;
   google_key: string;
 }
+const breadCrumbs = [
+  { name: 'Ангара77', path: '/' },
+  { name: 'Контакты', path: url.contacts() },
+];
 
 export default function About({ google_key }: IProps) {
   const classes = useStyles();
-  const breadCrumbs = [
-    { name: 'Ангара77', path: '/' },
-    { name: 'Контакты', path: url.contacts() },
-  ];
   return (
     <React.Fragment>
       <AboutHead />
@@ -239,31 +239,47 @@ export const getStaticProps: any = async (context: any) => {
   };
 };
 
-const AboutHead = () => (
-  <Head>
-    <title key="title">Контакты | Angara Parts</title>
-    <meta
-      key="description"
-      name="description"
-      content={`Angara 77 | ${footerData.SHOP_PHONE} Information about our
+const AboutHead = () => {
+  const breadItems = breadCrumbs.map((bread: IBread, i: number) => ({
+    '@type': 'ListItem',
+    position: i + 1,
+    name: bread.name,
+    item: `${SITE_DOMAIN_FULL}${bread.path}`,
+  }));
+  return (
+    <Head>
+      <title key="title">Контакты | Angara Parts</title>
+      <meta
+        key="description"
+        name="description"
+        content={`Angara 77 | ${footerData.SHOP_PHONE} Information about our
           company and history of establishment. We are open our dors in 2001 first time`}
-    />
-    <meta
-      key="og:title"
-      property="og:title"
-      content="Get your car in perfect health | Angara Parts | About Us"
-    />
-    <meta
-      key="og:url"
-      property="og:url"
-      content={`${SITE_DOMAIN_FULL}/about`}
-    />
-    <meta key="og:image" property="og:image" content="/favicon.png" />
-    <meta key="og:image:type" property="og:image:type" content="image/png" />
-    <meta key="og:image:width" property="og:image:width" content="1200" />
-    <meta key="og:image:hight" property="og:image:hight" content="630" />
-
-    <meta key="og:image:alt" property="og:image:alt" content="Angara 77 logo" />
-    <link rel="canonical" key="canonical" href={`${SITE_DOMAIN_FULL}/about`} />
-  </Head>
-);
+      />
+      <meta
+        key="og:title"
+        property="og:title"
+        content="Get your car in perfect health | Angara Parts | About Us"
+      />
+      <meta
+        key="og:url"
+        property="og:url"
+        content={`${SITE_DOMAIN_FULL}/about`}
+      />
+      <link
+        rel="canonical"
+        key="canonical"
+        href={`${SITE_DOMAIN_FULL}/about`}
+      />
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+            '@context': 'https://schema.org',
+            '@type': 'BreadcrumbList',
+            itemListElement: breadItems,
+          }),
+        }}
+      />
+    </Head>
+  );
+};
