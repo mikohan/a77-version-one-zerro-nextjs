a189f19d29db3180376abcae4abd61aba4c2c351
mikohan <angara99@gmail.com>
2021-07-04 18:11:06 +0300

    Server changed

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,8 @@
 import { ITransFilter, IFilter } from '~/interfaces';
 
 export const imageServerUrl = 'http://localhost:8000';
-export const backServerUrl = 'http://localhost:8000';
+//export const backServerUrl = 'http://localhost:8000';
+export const backServerUrl = 'https://angara77.ru';
 // export const backServerUrl = 'http://192.168.0.109:8000'; // url for elastic search
 export const backServerUrlRest = 'http://0.0.0.0:8000'; // url for elastic search
 
