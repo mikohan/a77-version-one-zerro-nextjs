15cf6fead424c9101922729177488835d104662b
mikohan <angara99@gmail.com>
2021-07-08 13:12:16 +0300

    Images fixed

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   images: {
-    domains: ['localhost', 'picsum.photos', 'angara77.ru'],
+    domains: ['localhost', 'picsum.photos', 'angara77.ru', 'angara77.ml'],
   },
   crossOrigin: 'anonymous',
   webpack(config) {
