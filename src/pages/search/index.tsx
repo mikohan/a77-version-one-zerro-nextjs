ed3915cb70fb2efed8b99a36c1761d46d301576a
mikohan <angara99@gmail.com>
2021-07-13 17:28:34 +0300

    Fixed get from cookies

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -24,319 +24,316 @@ import { getProductsBySearch } from '~/endpoints/productEndpoint';
 import { IActiveFilterMy } from '~/interfaces';
 import { Router } from 'next/dist/client/router';
 import {
-  shopProductLoading,
-  shopSetFilterVlue,
-  shopSetOldPrice,
+	shopProductLoading,
+	shopSetFilterVlue,
+	shopSetOldPrice,
 } from '~/store/shop/shopActions';
 import { containerMaxWidth, pageSize } from '~/config';
 import {
-  makeHandleDeleteFilter,
-  makeHandleDeleteFilters,
-  makeHandleFilterChange,
+	makeHandleDeleteFilter,
+	makeHandleDeleteFilters,
+	makeHandleFilterChange,
 } from '~/services/filters/filterHandler';
 import { createCheckFilters } from '~/services/filters/filterCreater';
 import { Container } from '@material-ui/core';
 
 interface CategoryProps {
-  category: IShopCategory;
-  categoriesForFilter: ICategory[];
-  categoryId?: number;
-  products: IProductElasticHitsFirst;
-  make: string;
-  model: ICar;
-  updated: Date;
-  catPath: ICategory[];
-  categories: ICategory[];
-  aggregations: IAgregations;
-  totalPages: number;
-  routerQuery: IRouterStuff;
-  routerParams: IRouterStuff;
+	category: IShopCategory;
+	categoriesForFilter: ICategory[];
+	categoryId?: number;
+	products: IProductElasticHitsFirst;
+	make: string;
+	model: ICar;
+	updated: Date;
+	catPath: ICategory[];
+	categories: ICategory[];
+	aggregations: IAgregations;
+	totalPages: number;
+	routerQuery: IRouterStuff;
+	routerParams: IRouterStuff;
 }
 import { capitalize } from '~/utils';
 import CarChoiser from '~/components/car/CarChoiserLong';
 import { Theme, makeStyles, createStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    container: {
-      margin: '0 auto',
-      [theme.breakpoints.down('lg')]: {
-        maxWidth: '85%',
-      },
-      [theme.breakpoints.up('xl')]: {
-        maxWidth: '80%',
-      },
-      [theme.breakpoints.up('xxl')]: {
-        maxWidth: '75%',
-      },
-      paddingBottom: theme.spacing(3),
-    },
-  })
+	createStyles({
+		container: {
+			margin: '0 auto',
+			[theme.breakpoints.down('lg')]: {
+				maxWidth: '85%',
+			},
+			[theme.breakpoints.up('xl')]: {
+				maxWidth: '80%',
+			},
+			[theme.breakpoints.up('xxl')]: {
+				maxWidth: '75%',
+			},
+			paddingBottom: theme.spacing(3),
+		},
+	})
 );
 
 export default function Cagetory(props: CategoryProps) {
-  const classes = useStyles();
-  const { products, aggregations, totalPages, routerQuery } = props;
+	const classes = useStyles();
+	const { products, aggregations, totalPages, routerQuery } = props;
 
-  const dispatch = useDispatch();
-  const router = useRouter();
-  Router.events.on('routeChangeStart', () => {
-    dispatch(shopProductLoading(true));
-  });
-  Router.events.on('routeChangeComplete', () => {
-    dispatch(shopProductLoading(false));
-  });
-  const filtersFromStore = useSelector(
-    (state: IState) => state.shopNew.filters
-  );
-  const search = routerQuery.search;
+	const dispatch = useDispatch();
+	const router = useRouter();
+	Router.events.on('routeChangeStart', () => {
+		dispatch(shopProductLoading(true));
+	});
+	Router.events.on('routeChangeComplete', () => {
+		dispatch(shopProductLoading(false));
+	});
+	const filtersFromStore = useSelector(
+		(state: IState) => state.shopNew.filters
+	);
+	const search = routerQuery.search;
 
-  const header = search ? `${capitalize(search)}` : '';
-  const count = products.total.value;
+	const header = search ? `${capitalize(search)}` : '';
+	const count = products.total.value;
 
-  /* const catBreads: IBread[] = orderedCatBreads?.map((item: ICategory) => ({ */
-  /*   name: item.name, */
-  /*   path: url.category(model.make.slug, model.slug, item.slug), */
-  /* })); */
+	/* const catBreads: IBread[] = orderedCatBreads?.map((item: ICategory) => ({ */
+	/*   name: item.name, */
+	/*   path: url.category(model.make.slug, model.slug, item.slug), */
+	/* })); */
 
-  const breads: IBread[] = [
-    { name: 'Ангара77', path: '/' },
-    { name: search, path: url.search(search) },
-  ];
+	const breads: IBread[] = [
+		{ name: 'Ангара77', path: '/' },
+		{ name: search, path: url.search(search) },
+	];
 
-  // Make array for brands filter
+	// Make array for brands filter
 
-  //************************ Filters Section Starts here**********************************************
+	//************************ Filters Section Starts here**********************************************
 
-  /* const categoriesFilter: IFilter = { */
-  /*   type: 'category', */
-  /*   name: 'Категории', */
-  /*   slug: 'category', */
-  /*   value: 'dvigatel', */
-  /*   path: orderedCatBreads, */
-  /*   items: categories, */
-  /* }; */
+	/* const categoriesFilter: IFilter = { */
+	/*   type: 'category', */
+	/*   name: 'Категории', */
+	/*   slug: 'category', */
+	/*   value: 'dvigatel', */
+	/*   path: orderedCatBreads, */
+	/*   items: categories, */
+	/* }; */
 
-  /* // ************************** Price filters ********************* */
-  let minPrice: number = 0;
-  let maxPrice: number = 0;
-  if (
-    aggregations.hasOwnProperty('min_price') &&
-    aggregations.hasOwnProperty('max_price')
-  ) {
-    minPrice = aggregations.min_price.value as number;
-    maxPrice = aggregations.max_price.value as number;
-  }
-  /* // Use effect for keeping price */
-  const oldPrice = useSelector(
-    (state: IState) => state.shopNew.filterPriceOldState
-  );
-  useEffect(() => {
-    if (oldPrice.length === 0) {
-      dispatch(shopSetOldPrice([minPrice, maxPrice]));
-    }
-  }, []);
+	/* // ************************** Price filters ********************* */
+	let minPrice: number = 0;
+	let maxPrice: number = 0;
+	if (
+		aggregations.hasOwnProperty('min_price') &&
+		aggregations.hasOwnProperty('max_price')
+	) {
+		minPrice = aggregations.min_price.value as number;
+		maxPrice = aggregations.max_price.value as number;
+	}
+	/* // Use effect for keeping price */
+	const oldPrice = useSelector(
+		(state: IState) => state.shopNew.filterPriceOldState
+	);
+	useEffect(() => {
+		if (oldPrice.length === 0) {
+			dispatch(shopSetOldPrice([minPrice, maxPrice]));
+		}
+	}, []);
 
-  const sortedFilters: IFilter[] = createCheckFilters(
-    router,
-    aggregations,
-    filtersFromStore,
-    oldPrice,
-    'search'
-  );
-  // ************************** End filters *********************
+	const sortedFilters: IFilter[] = createCheckFilters(
+		router,
+		aggregations,
+		filtersFromStore,
+		oldPrice,
+		'search'
+	);
+	// ************************** End filters *********************
 
-  const possibleFilters: string[] = sortedFilters.map(
-    (item: IFilter) => item.slug
-  );
+	const possibleFilters: string[] = sortedFilters.map(
+		(item: IFilter) => item.slug
+	);
 
-  // Getting filters from state redux
-  const activeFilters: IActiveFilterMy[] = [];
-  for (const [key, value] of Object.entries(routerQuery)) {
-    if (key !== 'page') {
-      activeFilters.push({
-        filterSlug: key,
-        filterValues: value.split(','),
-      });
-    }
-  }
+	// Getting filters from state redux
+	const activeFilters: IActiveFilterMy[] = [];
+	for (const [key, value] of Object.entries(routerQuery)) {
+		if (key !== 'page') {
+			activeFilters.push({
+				filterSlug: key,
+				filterValues: value.split(','),
+			});
+		}
+	}
 
-  /* function setModel() { */
-  /*   if (carSlug === 'портер1') { */
-  /*     if (!router.asPath.includes('car_models')) { */
-  /*       if (!router.asPath.includes('?')) { */
-  /*         router.push(`${router.asPath}?car_models=${carSlug}`); */
-  /*       } else { */
-  /*         router.push(`${router.asPath}&car_models=${carSlug}`); */
-  /*       } */
-  /*     } */
-  /*   } */
-  /* } */
+	/* function setModel() { */
+	/*   if (carSlug === 'портер1') { */
+	/*     if (!router.asPath.includes('car_models')) { */
+	/*       if (!router.asPath.includes('?')) { */
+	/*         router.push(`${router.asPath}?car_models=${carSlug}`); */
+	/*       } else { */
+	/*         router.push(`${router.asPath}&car_models=${carSlug}`); */
+	/*       } */
+	/*     } */
+	/*   } */
+	/* } */
 
-  /* const activeFilters: IActiveFilterMy[] = getActiveFilters( */
-  /*   /1* routerParams, *1/ */
-  /*   routerQuery, */
-  /*   filtersFromStore, */
-  /*   possibleFilters */
-  /* ); */
+	/* const activeFilters: IActiveFilterMy[] = getActiveFilters( */
+	/*   /1* routerParams, *1/ */
+	/*   routerQuery, */
+	/*   filtersFromStore, */
+	/*   possibleFilters */
+	/* ); */
 
-  // Putting filters from url to store
-  useEffect(() => {
-    if (!Object.keys(filtersFromStore).length) {
-      for (const filter of activeFilters) {
-        const fvalues = filter.filterValues.join(',');
-        dispatch(shopSetFilterVlue(filter.filterSlug, fvalues));
-      }
-    }
-  }, []);
+	// Putting filters from url to store
+	useEffect(() => {
+		if (!Object.keys(filtersFromStore).length) {
+			for (const filter of activeFilters) {
+				const fvalues = filter.filterValues.join(',');
+				dispatch(shopSetFilterVlue(filter.filterSlug, fvalues));
+			}
+		}
+	}, []);
 
-  /* // Function for redirection */
-  const handleFilterChange = makeHandleFilterChange(
-    activeFilters,
-    router,
-    dispatch,
-    undefined,
-    undefined,
-    search
-  );
+	/* // Function for redirection */
+	const handleFilterChange = makeHandleFilterChange(
+		activeFilters,
+		router,
+		dispatch,
+		undefined,
+		undefined,
+		search
+	);
 
-  const handleDeleteFilter = makeHandleDeleteFilter(
-    router,
-    dispatch,
-    activeFilters,
-    undefined,
-    undefined,
-    search
-  );
-  const handleDeleteFilters = makeHandleDeleteFilters(
-    router,
-    dispatch,
-    undefined,
-    undefined,
-    search
-  );
-  return (
-    <React.Fragment>
-      <SearchHead searchQuery={search} />
-      <AnimationPage>
-        <Box className={classes.container}>
-          <Grid container>
-            <PageHeader header={header} breads={breads} count={count} />
-            <Hidden smDown>
-              <Grid item xs={3}>
-                <LeftSideBar>
-                  <FilterWidget
-                    filters={sortedFilters}
-                    handleChange={handleFilterChange}
-                  />
-                </LeftSideBar>
-              </Grid>
-            </Hidden>
-            <Grid item xs={12} md={9}>
-              <Grid container>
-                <Grid item xs={12}>
-                  <CarChoiser size="sm" />
-                </Grid>
-                <Grid item xs={12}>
-                  {products.hits.length > 0 ? (
-                    <ShopGrid
-                      products={products.hits}
-                      totalPages={totalPages}
-                      filtersResetHandlers={{
-                        handleDeleteFilter,
-                        handleDeleteFilters,
-                      }}
-                    />
-                  ) : (
-                    <div>No stuff</div>
-                  )}
-                </Grid>
-              </Grid>
-            </Grid>
-          </Grid>
-        </Box>
-      </AnimationPage>
-    </React.Fragment>
-  );
+	const handleDeleteFilter = makeHandleDeleteFilter(
+		router,
+		dispatch,
+		activeFilters,
+		undefined,
+		undefined,
+		search
+	);
+	const handleDeleteFilters = makeHandleDeleteFilters(
+		router,
+		dispatch,
+		undefined,
+		undefined,
+		search
+	);
+	return (
+		<React.Fragment>
+			<SearchHead searchQuery={search} />
+			<AnimationPage>
+				<Box className={classes.container}>
+					<Grid container>
+						<PageHeader
+							header={header}
+							breads={breads}
+							count={count}
+						/>
+						<Hidden smDown>
+							<Grid item xs={3}>
+								<LeftSideBar>
+									<FilterWidget
+										filters={sortedFilters}
+										handleChange={handleFilterChange}
+									/>
+								</LeftSideBar>
+							</Grid>
+						</Hidden>
+						<Grid item xs={12} md={9}>
+							<Grid container>
+								<Grid item xs={12}>
+									<CarChoiser size="sm" />
+								</Grid>
+								<Grid item xs={12}>
+									{products.hits.length > 0 ? (
+										<ShopGrid
+											products={products.hits}
+											totalPages={totalPages}
+											filtersResetHandlers={{
+												handleDeleteFilter,
+												handleDeleteFilters,
+											}}
+										/>
+									) : (
+										<div>No stuff</div>
+									)}
+								</Grid>
+							</Grid>
+						</Grid>
+					</Grid>
+				</Box>
+			</AnimationPage>
+		</React.Fragment>
+	);
 }
 
 export const getServerSideProps: GetServerSideProps = async (
-  context: GetServerSidePropsContext
+	context: GetServerSidePropsContext
 ) => {
-  const routerQuery = context.query;
-  let carSlug: string = '';
-  try {
-    const cook: any = context.req;
-    const car = JSON.parse(cook.cookies.currentCar);
-    carSlug = car.slug;
-  } catch (e) {
-    console.log('Error in get car from cookies', e);
-  }
+	const routerQuery = context.query;
+	let carSlug: string = '';
 
-  // Probably needs to go ouside this file
-  // Cleaning filters from pages and main url params
-  const filtersQuery = Object.assign({}, routerQuery);
-  if (filtersQuery.hasOwnProperty('page')) {
-    delete filtersQuery.page;
-  }
-  // sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
-  // sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+	// Probably needs to go ouside this file
+	// Cleaning filters from pages and main url params
+	const filtersQuery = Object.assign({}, routerQuery);
+	if (filtersQuery.hasOwnProperty('page')) {
+		delete filtersQuery.page;
+	}
+	// sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+	// sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
 
-  // Comment out for building next time here and in static paths
-  //pagination part
-  const str: string = asString(context.query.page as string);
-  const page: number = parseInt(str) || 1;
+	// Comment out for building next time here and in static paths
+	//pagination part
+	const str: string = asString(context.query.page as string);
+	const page: number = parseInt(str) || 1;
 
-  const page_from = pageSize * (page - 1);
+	const page_from = pageSize * (page - 1);
 
-  let url = '';
+	let url = '';
 
-  if (Object.entries(filtersQuery).length > 0) {
-    let filUrl = '';
-    let amp = '&';
-    Object.entries(filtersQuery).forEach(([filter, value], i) => {
-      if (i === Object.entries(filtersQuery).length - 1) {
-        amp = '';
-      }
-      filUrl += `${filter}=${value}${amp}`;
-    });
-    url = `?${filUrl}&page_from=${page_from}&page_size=${pageSize}`;
-  } else {
-    url = `?&page_from=${page_from}&page_size=${pageSize}`;
-  }
+	if (Object.entries(filtersQuery).length > 0) {
+		let filUrl = '';
+		let amp = '&';
+		Object.entries(filtersQuery).forEach(([filter, value], i) => {
+			if (i === Object.entries(filtersQuery).length - 1) {
+				amp = '';
+			}
+			filUrl += `${filter}=${value}${amp}`;
+		});
+		url = `?${filUrl}&page_from=${page_from}&page_size=${pageSize}`;
+	} else {
+		url = `?&page_from=${page_from}&page_size=${pageSize}`;
+	}
 
-  /* if (carSlug) { */
-  /*   url += `${url}&car_models=портер1`; */
-  /* } */
-  // console.log(url);
-  const promise = await getProductsBySearch(url);
+	/* if (carSlug) { */
+	/*   url += `${url}&car_models=портер1`; */
+	/* } */
+	// console.log(url);
+	const promise = await getProductsBySearch(url);
 
-  const categories: IAggregationCategory[] =
-    promise.aggregations.categories.buckets;
-  let products: IProductElasticHitsFirst = promise.hits;
-  const prodCount: number = products.total.value;
+	const categories: IAggregationCategory[] =
+		promise.aggregations.categories.buckets;
+	let products: IProductElasticHitsFirst = promise.hits;
+	const prodCount: number = products.total.value;
 
-  const totalPages = Math.ceil(prodCount / pageSize);
+	const totalPages = Math.ceil(prodCount / pageSize);
 
-  const aggregations: IAgregations = promise.aggregations;
+	const aggregations: IAgregations = promise.aggregations;
 
-  const localCatTree: ICategory[] = makeTree(categories);
+	const localCatTree: ICategory[] = makeTree(categories);
 
-  if (!promise) {
-    return {
-      notFound: true,
-    };
-  }
-  return {
-    props: {
-      car: {},
-      /* category: cat, */
-      /* categories: catRet, */
-      products: products,
-      /* catPath: catPath, */
-      aggregations,
-      totalPages,
-      routerQuery,
-    },
-  };
+	if (!promise) {
+		return {
+			notFound: true,
+		};
+	}
+	return {
+		props: {
+			car: {},
+			/* category: cat, */
+			/* categories: catRet, */
+			products: products,
+			/* catPath: catPath, */
+			aggregations,
+			totalPages,
+			routerQuery,
+		},
+	};
 };
