da5553182726c19818cd47e15c0f286b2254487f
mikohan <angara99@gmail.com>
2021-07-20 09:33:51 +0300

    need to make xml for yandex

--- src/components/account/AddressesListing.tsx
+++ src/components/account/AddressesListing.tsx
@@ -10,7 +10,7 @@ import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
-// Comment
+
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     container: {
