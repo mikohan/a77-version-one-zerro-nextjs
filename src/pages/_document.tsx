2f254cb838ef935056ba302eca72702d8dd5bd18
mikohan <angara99@gmail.com>
2021-07-31 17:53:38 +0300

    Robots fixed

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,7 +16,6 @@ export default class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
-          <meta name="robots" content="noindex, nofollow" />
         </Head>
         <body>
           <Main />
