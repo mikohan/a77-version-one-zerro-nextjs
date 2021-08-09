4323b3db04f1509b30346361f15cd67d0b223ed5
mikohan <angara99@gmail.com>
2021-08-09 11:45:47 +0300

    Changed some stuff

--- src/store/users/userAction.ts
+++ src/store/users/userAction.ts
@@ -162,10 +162,10 @@ export const googleLogin = (tokenId: string) => async (
       },
     };
     const urlAxios = `${backServerUrl}/auth/social/google/`;
-    console.log(urlAxios);
     try {
       const res = await axios.post(urlAxios, payload);
       const response = res.data;
+      console.log(response);
       dispatch({
         type: USER_GOOGLE_LOGIN_SUCCESS,
         payload: response,
@@ -174,6 +174,7 @@ export const googleLogin = (tokenId: string) => async (
       dispatch(successMessageAction(res.data));
       Router.push(url.account.dashboard());
     } catch (e) {
+      console.log(e);
       dispatch(errorAction(e.response.data.errors));
 
       dispatch(successMessageAction(null));
