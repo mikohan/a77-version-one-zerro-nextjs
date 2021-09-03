9490feb4bf9c832d6e88a529d31ba9d9330a7af0
mikohan <angara99@gmail.com>
2021-09-03 11:20:08 +0300

    Removed crap

--- src/pages/product/[slug].tsx
+++ src/pages/product/[slug].tsx
@@ -30,7 +30,6 @@ import url from '~/services/url';
 import ProductGrid from '~/components/blog/ProductGrid';
 import RelatedPosts from '~/components/product/productPage/RelatedPosts';
 import Divider from '~/components/common/Divider';
-// Comment added
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
