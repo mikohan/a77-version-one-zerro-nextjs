a0080056960db503b1d604c5e3f6e2a1c37025ee
mikohan <angara99@gmail.com>
2021-09-03 11:19:44 +0300

    Added some shit

--- src/pages/product/[slug].tsx
+++ src/pages/product/[slug].tsx
@@ -30,6 +30,7 @@ import url from '~/services/url';
 import ProductGrid from '~/components/blog/ProductGrid';
 import RelatedPosts from '~/components/product/productPage/RelatedPosts';
 import Divider from '~/components/common/Divider';
+// Comment added
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
