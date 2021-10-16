ed5d35bff336e70f68536fdc86a25a7c61170529
mikohan <angara99@gmail.com>
2021-10-16 15:54:27 +0300

    Refactored Address Listing

--- src/components/account/AddressesListing.tsx
+++ src/components/account/AddressesListing.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Typography, Box, Button } from '@material-ui/core';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 import { Theme, makeStyles, createStyles } from '@material-ui/core/styles';
 import { IUser } from '~/interfaces';
 import { IAddress } from '~/interfaces';
