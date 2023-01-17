96401eeae2fbd8bfee50aaf9f3e338e31c698283
manhee <angara@gmail.com>
2023-01-17 12:00:52 +0100

    Added yandex govnometrika

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -35,6 +35,39 @@ export default class MyDocument extends Document {
               />
             </>
           )}
+          
+          {isProduction && (
+            <>
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+              />
+              <script
+                // eslint-disable-next-line react/no-danger
+                dangerouslySetInnerHTML={{
+                  __html: `
+                 <!-- Yandex.Metrika counter -->
+  <script type="text/javascript" >
+  (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+  m[i].l=1*new Date();
+  for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+  k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+  (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+  ym(55100605, "init", {
+  clickmap:true,
+  trackLinks:true,
+  accurateTrackBounce:true,
+  ecommerce:"dataLayer"
+  });
+  </script>
+  <noscript><div><img src="https://mc.yandex.ru/watch/55100605" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+  <!-- /Yandex.Metrika counter --> 
+          `,
+                }}
+              />
+            </>
+          )}
 
           <meta name="theme-color" content={theme.palette.primary.main} />
           <link rel="shortcut icon" href="/favicon.png" />
