d1d640c7a5cc780f762feba5f77d5cf5040485c5
manhee <angara@gmail.com>
2023-01-17 14:41:03 +0100

    Added metrika

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -38,6 +38,7 @@ import { shopSetUserId } from '~/store/shop/shopActions';
 import { createOrUpdateUser } from '~/endpoints/carsEndpoint';
 // import SimpleReactLightbox from 'simple-react-lightbox';
 import * as gtag from '~/services/gtag';
+import Script from "next/script"
 
 Router.events.on('routeChangeStart', () => {
   NProgress.start();
@@ -159,6 +160,26 @@ function MyApp(props: any) {
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+        <Script
+          id='yandex-metrika'
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+            ym(55100605, "init", {
+              clickmap:true,
+              trackLinks:true,
+            accurateTrackBounce:true,
+            ecommerce:"dataLayer"
+            });
+            `,
+          }}
         />
       </Head>
       <CookiesProvider>
